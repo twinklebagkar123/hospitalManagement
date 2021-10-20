3a500de1f5c8da3ba8a3e2178d65cfee38971fe2
twinklebagkar123 <twinklebagkar99@gmail.com>
2021-10-20 17:25:01 +0530

    config

--- hms/admin/include/config.php
+++ hms/admin/include/config.php
@@ -1,6 +1,6 @@
 <?php
 define('DB_SERVER','localhost');
-define('DB_USER','u298126064_hospitaloot');
+define('DB_USER','u298126064_hospital');
 define('DB_PASS' ,'?mnR!Y!y0');
 define('DB_NAME', 'u298126064_hospital');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
