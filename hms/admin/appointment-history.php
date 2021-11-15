c43dca2bb1e88a3e00213b008bf9de8f6e7f44f3
twinklebagkar123 <twinklebagkar99@gmail.com>
2021-11-15 15:54:39 +0530

    query

--- hms/admin/appointment-history.php
+++ hms/admin/appointment-history.php
@@ -82,11 +82,12 @@ if(isset($_GET['cancel']))
 										</thead>
 										<tbody>
 <?php
-$sql=mysqli_query($con,"SELECT `tblp.PatientName`,`doc.doctorName`,`doc.specilization`,`doc.docFees`,`apt.appointmentDate`,`apt.postingDate` FROM appointment as apt INNER JOIN tblpatient AS tblp ON apt.userId = tblp.ID INNER JOIN doctors AS doc ON apt.doctorId = doc.id");
+$sql="SELECT `tblp.PatientName`,`doc.doctorName`,`doc.specilization`,`doc.docFees`,`apt.appointmentDate`,`apt.postingDate` FROM appointment as apt INNER JOIN tblpatient AS tblp ON apt.userId = tblp.ID INNER JOIN doctors AS doc ON apt.doctorId = doc.id";
+$result = $con->query($sql);
 $cnt=1;
-print($sql);
-while($row=mysqli_fetch_array($sql))
-var_dump($row);
+//print($sql);
+while($row = mysqli_fetch_array($result))
+//var_dump($row);
 {
 ?>
 
