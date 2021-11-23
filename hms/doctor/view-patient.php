bb4ed80db89be52fe9c6000ffde1535456f7c8ce
Bhaveshbagkar09 <90370475+Bhaveshbagkar09@users.noreply.github.com>
2021-11-23 16:08:58 +0530

    Update view-patient.php

--- hms/doctor/view-patient.php
+++ hms/doctor/view-patient.php
@@ -32,26 +32,26 @@ if(isset($_POST['submit']))
 ?>
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Doctor | Manage Patients</title>
-		
-		<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
-		<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-		<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
-		<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
-		<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
-		<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
-		<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
-		<link href="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
-		<link href="vendor/select2/select2.min.css" rel="stylesheet" media="screen">
-		<link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
-		<link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
-		<link rel="stylesheet" href="assets/css/styles.css">
-		<link rel="stylesheet" href="assets/css/styleext.css">
-		<link rel="stylesheet" href="assets/css/plugins.css">
-		<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
-	</head>
-	<body>
+  <head>
+    <title>Doctor | Manage Patients</title>
+    
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+    <link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+    <link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+    <link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+    <link href="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="screen">
+    <link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
+    <link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/styleext.css">
+    <link rel="stylesheet" href="assets/css/plugins.css">
+    <link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+  </head>
+  <body>
     <script type="text/javascript">
       function getAllValues(){
         $("#loaderIcon").show();
@@ -70,13 +70,13 @@ if(isset($_POST['submit']))
 
   
   </script>
-		<div id="app">		
+    <div id="app">    
 <?php include('include/sidebar.php');?>
 <div class="app-content">
 <?php include('include/header.php');?>
 <div class="main-content" >
 <div class="wrap-content container" id="container">
-						<!-- start: PAGE TITLE -->
+            <!-- start: PAGE TITLE -->
 <section id="page-title">
 <div class="row">
 <div class="col-sm-8">
@@ -103,7 +103,7 @@ $cnt=1;
 while ($row=mysqli_fetch_array($ret)) {
                                ?>
 <table style="border:1" class="table table-bordered">
- <tr style="align:center">
+ <tr style="align : center">
 <td colspan="4" style="font-size:20px;color:blue">
  Patient Details</td></tr>
 
@@ -179,7 +179,78 @@ while ($row=mysqli_fetch_array($ret)) {
 
 
 
+ <?php  
+$chr=mysqli_query($con,"SELECT DISTINCT `BloodSugar` FROM `tblmedicalhistory` WHERE BSType='Type1' AND PatientID='$vid'");
+$cart = array();
+while ($row=mysqli_fetch_array($chr)) { 
+  $value = $row['BloodSugar'];
 
+array_push($cart, $value);
+
+}
+
+
+ ?>
+ <?php  
+$chr=mysqli_query($con,"SELECT DISTINCT `BloodSugar` FROM `tblmedicalhistory` WHERE BSType='Type2' AND PatientID='$vid'");
+$c = array();
+while ($row=mysqli_fetch_array($chr)) { 
+  $v = $row['BloodSugar'];
+
+array_push($c, $v);
+
+}
+
+
+ ?>
+ <?php  
+$chr=mysqli_query($con,"SELECT DISTINCT BloodSugar FROM tblmedicalhistory WHERE BSType='Type3' AND PatientID='$vid'");
+$thirdd = array();
+while ($row=mysqli_fetch_array($chr)) { 
+  $vv = $row['BloodSugar'];
+
+array_push($thirdd, $vv);
+
+}
+
+
+ ?>
+ <?php  
+$chr=mysqli_query($con,"SELECT DISTINCT `BloodSugar` FROM `tblmedicalhistory` WHERE BSType='Type4' AND PatientID='$vid'");
+$fourth = array();
+while ($row=mysqli_fetch_array($chr)) { 
+  $vv = $row['BloodSugar'];
+
+array_push($fourth, $vv);
+
+}
+
+
+ ?>
+ <?php  
+$chr=mysqli_query($con,"SELECT DISTINCT `BloodSugar` FROM `tblmedicalhistory` WHERE BSType='Type5' AND PatientID='$vid'");
+$fifth = array();
+while ($row=mysqli_fetch_array($chr)) { 
+  $vv = $row['BloodSugar'];
+
+array_push($fifth, $vv);
+
+}
+
+
+ ?>
+  <?php  
+$chr=mysqli_query($con,"SELECT DISTINCT `BloodSugar` FROM `tblmedicalhistory` WHERE BSType='Type6' AND PatientID='$vid'");
+$sixth = array();
+while ($row=mysqli_fetch_array($chr)) { 
+  $vv = $row['BloodSugar'];
+
+array_push($sixth, $vv);
+
+}
+
+
+ ?>
 <canvas id="line-chart" width="400" height="100"></canvas>
 
 
@@ -288,79 +359,89 @@ while ($row=mysqli_fetch_array($ret)) {
 </div>
 </div>
 </div>
-			<!-- start: FOOTER -->
-	<?php include('include/footer.php');?>
-			<!-- end: FOOTER -->
-		
-			<!-- start: SETTINGS -->
-	<?php include('include/setting.php');?>
-			
-			<!-- end: SETTINGS -->
-		</div>
-		<!-- start: MAIN JAVASCRIPTS -->
-		<script src="vendor/jquery/jquery.min.js"></script>
-		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-		<script src="vendor/modernizr/modernizr.js"></script>
-		<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
-		<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-		<script src="vendor/switchery/switchery.min.js"></script>
-		<!-- end: MAIN JAVASCRIPTS -->
-		<!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
-		<script src="vendor/maskedinput/jquery.maskedinput.min.js"></script>
-		<script src="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
-		<script src="vendor/autosize/autosize.min.js"></script>
-		<script src="vendor/selectFx/classie.js"></script>
-		<script src="vendor/selectFx/selectFx.js"></script>
-		<script src="vendor/select2/select2.min.js"></script>
-		<script src="vendor/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
-		<script src="vendor/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
+      <!-- start: FOOTER -->
+  <?php include('include/footer.php');?>
+      <!-- end: FOOTER -->
+    
+      <!-- start: SETTINGS -->
+  <?php include('include/setting.php');?>
+      
+      <!-- end: SETTINGS -->
+    </div>
+    <!-- start: MAIN JAVASCRIPTS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="vendor/modernizr/modernizr.js"></script>
+    <script src="vendor/jquery-cookie/jquery.cookie.js"></script>
+    <script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="vendor/switchery/switchery.min.js"></script>
+    <!-- end: MAIN JAVASCRIPTS -->
+    <!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+    <script src="vendor/maskedinput/jquery.maskedinput.min.js"></script>
+    <script src="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+    <script src="vendor/autosize/autosize.min.js"></script>
+    <script src="vendor/selectFx/classie.js"></script>
+    <script src="vendor/selectFx/selectFx.js"></script>
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+    <script src="vendor/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
     <script
 src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
 </script>
-		<!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
-		<!-- start: CLIP-TWO JAVASCRIPTS -->
-		<script src="assets/js/main.js"></script>
-		<!-- start: JavaScript Event Handlers for this page -->
-		<script src="assets/js/form-elements.js"></script>
-		<script src="assets/js/doctor.js"></script>
-		<script>
-			jQuery(document).ready(function() {
-				Main.init();
-				FormElements.init();
+    <!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+    <!-- start: CLIP-TWO JAVASCRIPTS -->
+    <script src="assets/js/main.js"></script>
+    <!-- start: JavaScript Event Handlers for this page -->
+    <script src="assets/js/form-elements.js"></script>
+    <script src="assets/js/doctor.js"></script>
+    <script>
+      jQuery(document).ready(function() {
+        Main.init();
+        FormElements.init();
         console.log("hello");
         
-			});
-		</script>
+      });
+    </script>
     
 <script>
 new Chart(document.getElementById("line-chart"), {
   type: 'line',
   data: {
-    labels: [1500,1600,1700,1750,1800,1850,1900,1950,1999,2050],
+    labels: [10,20,30,40,50,60,70,80,90,100,110,120,130],
     datasets: [{ 
-        data: [86,114,106,106,107,111,133,221,783,2478],
-        label: "Africa",
+        data: [<?php foreach($cart as $val){echo $val; echo ",";}?>],
+        label: "Type1",
         borderColor: "#3e95cd",
         fill: false
-      }, { 
-        data: [282,350,411,502,635,809,947,1402,3700,5267],
-        label: "Asia",
+      },
+      { 
+        data: [<?php foreach($c as $val){echo $val; echo ",";}?>],
+        label: "Type2",
         borderColor: "#8e5ea2",
         fill: false
-      }, { 
-        data: [168,170,178,190,203,276,408,547,675,734],
-        label: "Europe",
+      }
+      , { 
+         data: [<?php foreach($thirdd as $val){echo $val; echo ",";}?>],
+        label: "Type3",
         borderColor: "#3cba9f",
         fill: false
-      }, { 
-        data: [40,20,10,16,24,38,74,167,508,784],
-        label: "Latin America",
-        borderColor: "#e8c3b9",
+      }
+      , { 
+         data: [<?php foreach($fourth as $val){echo $val; echo ",";}?>],
+        label: "Type4",
+        borderColor: "#ffff00",
         fill: false
-      }, { 
-        data: [6,3,2,2,7,26,82,172,312,433],
-        label: "North America",
-        borderColor: "#c45850",
+      }
+      , { 
+         data: [<?php foreach($fifth as $val){echo $val; echo ",";}?>],
+        label: "Type5",
+        borderColor: "#ff0000",
+        fill: false
+      }
+      , { 
+         data: [<?php foreach($sixth as $val){echo $val; echo ",";}?>],
+        label: "Type6",
+        borderColor: "#99ff33",
         fill: false
       }
     ]
@@ -368,12 +449,12 @@ new Chart(document.getElementById("line-chart"), {
   options: {
     title: {
       display: true,
-      text: 'World population per region (in millions)'
+      text: 'Blood Sugar'
     }
   }
 });
 </script>
-		<!-- end: JavaScript Event Handlers for this page -->
-		<!-- end: CLIP-TWO JAVASCRIPTS -->
-	</body>
+    <!-- end: JavaScript Event Handlers for this page -->
+    <!-- end: CLIP-TWO JAVASCRIPTS -->
+  </body>
 </html>
