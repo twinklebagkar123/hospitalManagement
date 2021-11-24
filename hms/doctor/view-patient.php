8f7517e280ca9468684c8dfccc696952f10b5f81
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-11-24 16:41:48 +0530

    print type

--- hms/doctor/view-patient.php
+++ hms/doctor/view-patient.php
@@ -189,20 +189,20 @@ while($row= $result->fetch_assoc())
 $i=1;
 $type = $row["BSType"];
 print_r($type);
-if($type != ""){
-  $query2 = "SELECT  `BloodSugar` FROM `tblmedicalhistory` WHERE BSType='".$type."' AND PatientID='$vid'";
- // print_r($query2);
-  $result1 = $con->query($query2);
-  while($row2=$result1-> fetch_assoc())
-{
-$value = $row2["BloodSugar"];
-var_dump($value);
-$data[$type][] = array_push($value);
-//$data [] = array_push($value);
+// if($type != ""){
+//   $query2 = "SELECT  `BloodSugar` FROM `tblmedicalhistory` WHERE BSType='".$type."' AND PatientID='$vid'";
+//  // print_r($query2);
+//   $result1 = $con->query($query2);
+//   while($row2=$result1-> fetch_assoc())
+// {
+// $value = $row2["BloodSugar"];
+// var_dump($value);
+// $data[$type][] = array_push($value);
+// //$data [] = array_push($value);
 
-}
+// }
 
-}
+// }
 
 // 
 
