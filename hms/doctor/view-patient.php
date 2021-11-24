9dc3fd744a768e81c5450faf090197a7ece80c5b
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-11-24 16:39:20 +0530

    check type

--- hms/doctor/view-patient.php
+++ hms/doctor/view-patient.php
@@ -188,7 +188,7 @@ while($row= $result->fetch_assoc())
 {
 $i=1;
 $type = $row["BSType"];
-//print_r($type);
+print_r($type);
 if($type != ""){
   $query2 = "SELECT  `BloodSugar` FROM `tblmedicalhistory` WHERE BSType='".$type."' AND PatientID='$vid'";
  // print_r($query2);
