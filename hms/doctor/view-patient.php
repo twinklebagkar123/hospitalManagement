5bc676bd4580b5ac77ab0724cf162901005f1030
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-11-29 17:11:13 +0530

    update version

--- hms/doctor/view-patient.php
+++ hms/doctor/view-patient.php
@@ -43,7 +43,7 @@ if (isset($_POST['submit'])) {
   <link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
   <link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
   <link rel="stylesheet" href="assets/css/styles.css">
-  <link rel="stylesheet" href="assets/css/styleext.css">
+  <link rel="stylesheet" href="assets/css/styleext.css?ver=<?php echo rand();?>">
   <link rel="stylesheet" href="assets/css/plugins.css">
   <link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
 </head>
