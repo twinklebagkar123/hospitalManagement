0a4b86119593dbd3445e888eb2f63f6fcafba465
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-11-30 12:30:40 +0530

    check

--- hms/doctor/view-patient.php
+++ hms/doctor/view-patient.php
@@ -52,7 +52,7 @@ if (isset($_POST['submit'])) {
   <script type="text/javascript">
     function getAllValues() {
       $("#loaderIcon").show();
-      if(('#autosuggest').val()==""){
+      if($('#autosuggest').val() == ""){
         $("#pillResult").html(' ');
       }
       else{
