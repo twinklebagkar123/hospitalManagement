a844d361eaf28d2b73a8220b5914b68eceb60f6b
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-12-02 16:57:59 +0530

    check

--- hms/admin/view-patient.php
+++ hms/admin/view-patient.php
@@ -232,7 +232,7 @@ if (isset($_POST['submit'])) {
 			new Chart(document.getElementById("line-chart"), {
           type: 'line',
           data: {
-            labels: [<?php for($i=0;$i<150;$i++){echo $i.',';}?>],
+            labels: [<?php for($i=0;$i<150;$i++){echo $i.'-11-2021,';}?>],
             datasets: [
               <?php
 
