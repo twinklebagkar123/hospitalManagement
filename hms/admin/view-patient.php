4e248906b444f74582c111381821c1037134b480
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-12-02 17:14:27 +0530

    check

--- hms/admin/view-patient.php
+++ hms/admin/view-patient.php
@@ -238,7 +238,7 @@ if (isset($_POST['submit'])) {
 			new Chart(document.getElementById("line-chart"), {
           type: 'line',
           data: {
-            labels: [<?php   foreach ($period as $key => $value) {print_r($value->format('Y-m-d') ); echo ","; }?>],
+            labels: [<?php   foreach ($period as $key => $value) { echo "'",print_r($value->format('Y-m-d') ); echo "',"; }?>],
             datasets: [
               <?php
 
