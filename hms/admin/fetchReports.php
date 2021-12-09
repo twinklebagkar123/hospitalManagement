2f31e330dc7df7697690ae2a942b41b06de44545
twinklebagkar123 <twinklebagkar99@gmail.com>
2021-12-09 16:50:55 +0530

    admission

--- hms/admin/fetchReports.php
+++ hms/admin/fetchReports.php
@@ -12,8 +12,8 @@ if(!empty($_POST['admissionid'])){
 			new DateTime($dischargeDate)
 	   );
        $bsDates = array();
-       echo $admissionDate;
-       echo $dischargeDate;
+       echo $admissionDate."admission";
+       echo $dischargeDate."discharge";
 	   foreach ($period as $key => $value) { array_push($bsDates,$value->format('Y-m-d')); }
     //medical History 
     $query = "SELECT * FROM `tblmedicalhistory` WHERE admissionID = '$admissionid'";
