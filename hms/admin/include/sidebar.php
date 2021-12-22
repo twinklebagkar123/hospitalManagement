952721d44e4fc16b4dcd21d8f8032992e2bfdc0a
twinklebagkar123 <twinklebagkar99@gmail.com>
2021-12-22 16:41:01 +0530

    sidebar

--- hms/admin/include/sidebar.php
+++ hms/admin/include/sidebar.php
@@ -1,112 +1,112 @@
 <div class="sidebar app-aside" id="sidebar">
-				<div class="sidebar-container perfect-scrollbar">
+	<div class="sidebar-container perfect-scrollbar">
 
-<nav>
-						
-						<!-- start: MAIN NAVIGATION MENU -->
-						<div class="navbar-title">
-							<span>Main Navigation</span>
+		<nav>
+
+			<!-- start: MAIN NAVIGATION MENU -->
+			<div class="navbar-title">
+				<span>Main Navigation</span>
+			</div>
+			<ul class="main-navigation-menu">
+				<li>
+					<a href="dashboard.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-home"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Dashboard </span>
+							</div>
 						</div>
-						<ul class="main-navigation-menu">
-							<li>
-								<a href="dashboard.php">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-home"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> Dashboard </span>
-										</div>
-									</div>
-								</a>
-							</li>
-							<li>
-								<a href="javascript:void(0)">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-user"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> Doctors </span><i class="icon-arrow"></i>
-										</div>
-									</div>
-								</a>
-								<ul class="sub-menu">
-									<li>
-										<a href="doctor-specilization.php">
-											<span class="title"> Doctor Specialization </span>
-										</a>
-									</li>
-									<li>
-										<a href="add-doctor.php">
-											<span class="title"> Add Doctor</span>
-										</a>
-									</li>
-									<li>
-										<a href="Manage-doctors.php">
-											<span class="title"> Manage Doctors </span>
-										</a>
-									</li>
-									
-								</ul>
-								</li>
+					</a>
+				</li>
+				<li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-user"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Doctors </span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+						<li>
+							<a href="doctor-specilization.php">
+								<span class="title"> Doctor Specialization </span>
+							</a>
+						</li>
+						<li>
+							<a href="add-doctor.php">
+								<span class="title"> Add Doctor</span>
+							</a>
+						</li>
+						<li>
+							<a href="Manage-doctors.php">
+								<span class="title"> Manage Doctors </span>
+							</a>
+						</li>
+
+					</ul>
+				</li>
 
 				<li>
-								<a href="javascript:void(0)">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-user"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> Users </span><i class="icon-arrow"></i>
-										</div>
-									</div>
-								</a>
-								<ul class="sub-menu">
-									
-									<li>
-										<a href="manage-users.php">
-											<span class="title"> Manage Users </span>
-										</a>
-									</li>
-									
-								</ul>
-								</li>
-								<li>
-								<a href="javascript:void(0)">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-user"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> Patients </span><i class="icon-arrow"></i>
-										</div>
-									</div>
-								</a>
-								<ul class="sub-menu">
-									<li>
-										<a href="manage-patient.php">
-											<span class="title"> Manage Patients </span>
-										</a>
-									</li>
-									<li>
-										<a href="user-registration.php">
-											<span class="title"> Register Patient</span>
-										</a>
-									</li>
-									<li>
-										<a href="patient-admission.php">
-											<span class="title"> Patient Admission</span>
-										</a>
-									</li>
-									<!-- <li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-user"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Users </span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+
+						<li>
+							<a href="manage-users.php">
+								<span class="title"> Manage Users </span>
+							</a>
+						</li>
+
+					</ul>
+				</li>
+				<li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-user"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Patients </span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+						<li>
+							<a href="manage-patient.php">
+								<span class="title"> Manage Patients </span>
+							</a>
+						</li>
+						<li>
+							<a href="user-registration.php">
+								<span class="title"> Register Patient</span>
+							</a>
+						</li>
+						<li>
+							<a href="patient-admission.php">
+								<span class="title"> Patient Admission</span>
+							</a>
+						</li>
+						<!-- <li>
 										<a href="manage-patient.php">
 											<span class="title"> Manage Patients </span>
 										</a>
 									</li> -->
-									
-								</ul>
-								<!-- <ul class="sub-menu">
+
+					</ul>
+					<!-- <ul class="sub-menu">
 									
 									<li>
 										<a href="manage-patient.php">
@@ -115,178 +115,190 @@
 									</li>
 									
 								</ul> -->
-								</li>	
+				</li>
 
-<li>
-								<a href="appointment-history.php">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-file"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> Appointment History </span>
-										</div>
-									</div>
-								</a>
-							</li>
+				<li>
+					<a href="appointment-history.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-file"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Appointment History </span>
+							</div>
+						</div>
+					</a>
+				</li>
 
 
 
 				<li>
-								<a href="javascript:void(0)">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-files"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> Conatctus Queries </span><i class="icon-arrow"></i>
-										</div>
-									</div>
-								</a>
-								<ul class="sub-menu">
-									
-									<li>
-										<a href="unread-queries.php">
-											<span class="title"> Unread Query </span>
-										</a>
-									</li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-files"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Conatctus Queries </span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
 
-									<li>
-										<a href="read-query.php">
-											<span class="title"> Read Query </span>
-										</a>
-									</li>
-									
-								</ul>
-								</li>
+						<li>
+							<a href="unread-queries.php">
+								<span class="title"> Unread Query </span>
+							</a>
+						</li>
 
+						<li>
+							<a href="read-query.php">
+								<span class="title"> Read Query </span>
+							</a>
+						</li>
 
-
-	<li>
-								<a href="doctor-logs.php">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-list"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> Doctor Session Logs </span>
-										</div>
-									</div>
-								</a>
-							</li>		
+					</ul>
+				</li>
 
 
 
-							<li>
-								<a href="user-logs.php">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-list"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> User Session Logs </span>
-										</div>
-									</div>
-								</a>
-							</li>						
 				<li>
-								<a href="javascript:void(0)">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-files"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> Reports </span><i class="icon-arrow"></i>
-										</div>
-									</div>
-								</a>
-								<ul class="sub-menu">
-									
-									<li>
-										<a href="between-dates-reports.php">
-											<span class="title">B/w dates reports </span>
-										</a>
-									</li>
+					<a href="doctor-logs.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-list"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Doctor Session Logs </span>
+							</div>
+						</div>
+					</a>
+				</li>
 
-									
-									
-								</ul>
-								<li>
-								<a href="patient-search.php">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-search"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> Patient Search </span>
-										</div>
-									</div>
-								</a>
-							</li>
-								</li>
-								<li>
-								<a href="javascript:void(0)">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-files"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> Medicine </span><i class="icon-arrow"></i>
-										</div>
-									</div>
-								</a>
-								<ul class="sub-menu">
-									
-									<li>
-										<a href="add-medicine.php">
-											<span class="title"> Add medicine </span>
-										</a>
-									</li>
 
-									
-									
-								</ul>
-								</li>
-								<li>
-								<a href="nearbyAmbulance.php">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-list"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> Ambulance </span>
-										</div>
-									</div>
-								</a>
-							</li>
-								<li>
-								<a href="bill.php">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-list"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> Billing </span>
-										</div>
-									</div>
-								</a>
-							</li>						
-							<li>
-								<a href="patientOperationConsent.php">
-									<div class="item-content">
-										<div class="item-media">
-											<i class="ti-list"></i>
-										</div>
-										<div class="item-inner">
-											<span class="title"> Consent Form </span>
-										</div>
-									</div>
-								</a>
-							</li>	
 
-						</ul>
-						<!-- end: CORE FEATURES -->
-						
-					</nav>
-					</div>
-			</div>
+				<li>
+					<a href="user-logs.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-list"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> User Session Logs </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-files"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Reports </span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+
+						<li>
+							<a href="between-dates-reports.php">
+								<span class="title">B/w dates reports </span>
+							</a>
+						</li>
+
+
+
+					</ul>
+				<li>
+					<a href="patient-search.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-search"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Patient Search </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				</li>
+				<li>
+					<a href="javascript:void(0)">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-files"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Medicine </span><i class="icon-arrow"></i>
+							</div>
+						</div>
+					</a>
+					<ul class="sub-menu">
+
+						<li>
+							<a href="add-medicine.php">
+								<span class="title"> Add medicine </span>
+							</a>
+						</li>
+
+
+
+					</ul>
+				</li>
+				<li>
+					<a href="nearbyAmbulance.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-list"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Ambulance </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="bill.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-list"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Billing </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="addProcedure.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-search"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Procedure Section </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
+					<a href="patientOperationConsent.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-list"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Consent Form </span>
+							</div>
+						</div>
+					</a>
+				</li>
+
+			</ul>
+			<!-- end: CORE FEATURES -->
+
+		</nav>
+	</div>
+</div>
