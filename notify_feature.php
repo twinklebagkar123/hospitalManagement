9d79dbd2ecdb90c2e52b0f1f5c3a04d07dadfa50
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-12-28 11:05:03 +0530

    test mail php

--- notify_feature.php
+++ notify_feature.php
@@ -16,6 +16,26 @@
             </div>
         </div>
     </main>
+    <?php 
+        $to = "porobjagannath@gmail.com";
+        $subject = "This is subject";
+        
+        $message = "<b>This is HTML message.</b>";
+        $message .= "<h1>This is headline.</h1>";
+        
+        $header = "From:info@weblozee.com \r\n";
+        // $header .= "Cc:afgh@somedomain.com \r\n";
+        $header .= "MIME-Version: 1.0\r\n";
+        $header .= "Content-type: text/html\r\n";
+        
+        $retval = mail ($to,$subject,$message,$header);
+        
+        if( $retval == true ) {
+           echo "Message sent successfully...";
+        }else {
+           echo "Message could not be sent...";
+        }
+    ?>
     <script src="hms/vendor/jquery/jquery.min.js"></script>
     <script src="hms/vendor/bootstrap/js/bootstrap.min.js"></script>
     <script>
