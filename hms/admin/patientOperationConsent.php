ce625e41cefa41ceb5864b41a83b1d98bc25ddac
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-12-29 10:56:31 +0530

    css version dynamic

--- hms/admin/patientOperationConsent.php
+++ hms/admin/patientOperationConsent.php
@@ -51,7 +51,7 @@ $today = $year . '-' . $month . '-' . $day;
 	<link href="vendor/select2/select2.min.css" rel="stylesheet" media="screen">
 	<link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
 	<link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
-	<link rel="stylesheet" href="assets/css/styles.css">
+	<link rel="stylesheet" href="assets/css/styles.css?ver=<?php echo rand(); ?>" type="text/css">
 	<link rel="stylesheet" href="assets/css/plugins.css">
 	<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
 
