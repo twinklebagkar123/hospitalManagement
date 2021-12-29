8d450ba5e79ffd3e88305387d6010604f936908a
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2021-12-29 12:42:49 +0530

    Update listByDate.php

--- hms/admin/listByDate.php
+++ hms/admin/listByDate.php
@@ -178,7 +178,7 @@ $today = $year . '-' . $month . '-' . $day;
         FormElements.init();
 
         $("#opDate").on("change", function() {
-            console.log(helllo);
+            console.log('helllo');
         });
       });
 
