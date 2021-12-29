0519f167b8acae5096a2573be098dad7b55bc430
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2021-12-29 11:26:44 +0530

    querry

--- hms/admin/operationList.php
+++ hms/admin/operationList.php
@@ -113,7 +113,7 @@ $today = $year . '-' . $month . '-' . $day;
 												<td><?php echo $row['opTitle']; ?></td>
 												<td><?php echo $row['opTime']; ?></td>
 												<td><?php echo $row['pRNote']; ?>
-                        <td><?php echo $row['Doctor Name']; ?>
+                        <td><?php echo $row['doctorName']; ?>
 												</td>
 
 												
