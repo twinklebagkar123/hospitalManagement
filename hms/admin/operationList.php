65eab2a91855e27bb96cc2ed90c22e5968d4ab29
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2021-12-29 11:12:26 +0530

    Update operationList.php

--- hms/admin/operationList.php
+++ hms/admin/operationList.php
@@ -100,7 +100,7 @@ $today = $year . '-' . $month . '-' . $day;
 
 										<?php
 
-										$sql = mysqli_query($con, "SELECT patientoperation.patNme, patientoperation.opDate,patientoperation.opTitle,patientoperation.opTime,patientoperation.pRNote, doctors.doctorName FROM patientoperation INNER JOIN doctors ON patientoperation.docID = doctors.id AND patientoperation.opDate=‘$today’");
+										$sql = mysqli_query($con, "SELECT patientoperation.patNme, patientoperation.opDate,patientoperation.opTitle,patientoperation.opTime,patientoperation.pRNote, doctors.doctorName FROM patientoperation INNER JOIN doctors ON patientoperation.docID = doctors.id AND patientoperation.opDate='$today' ");
              
 										$cnt = 1;
 										while ($row = mysqli_fetch_array($sql)) {
