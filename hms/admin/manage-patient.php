5bb6c861d3506605af07480321b7fc9b95f8d3a8
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2021-12-30 13:09:39 +0530

    Pagination

--- hms/admin/manage-patient.php
+++ hms/admin/manage-patient.php
@@ -117,8 +117,36 @@ if (isset($_POST['submit'])) {
 									</thead>
 									<tbody id="patientList">
 
-									
-																			</tbody>
+										<?php
+
+										$sql = mysqli_query($con, "SELECT * FROM `tblpatient` WHERE `ID` >= 10 ORDER BY `ID` ASC LIMIT 10;");
+										$cnt = 1;
+										while ($row = mysqli_fetch_array($sql)) {
+										?>
+											<tr>
+												<td class="center"><?php echo $cnt; ?>.</td>
+												<td class="hidden-xs"><?php echo $row['PatientName']; ?></td>
+												<td><?php echo $row['PatientContno']; ?></td>
+												<td><?php echo $row['PatientGender']; ?></td>
+												<td><?php echo $row['CreationDate']; ?></td>
+												<td><?php echo $row['UpdationDate']; ?>
+												</td>
+
+												<td><button type="button" data-pid="<?php echo $row['ID']; ?>" data-name="<?php echo $row['PatientName']; ?>" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+														Book
+													</button></td>
+
+
+												<td>
+
+													<a href="view-patient.php?viewid=<?php echo $row['ID']; ?>"><i class="fa fa-eye"></i></a>
+
+												</td>
+											</tr>
+										<?php
+											$cnt = $cnt + 1;
+										} ?>
+									</tbody>
 
 
 							</div>
