bcbd3c0ee1592244337a85af3d6c86ce1ee967b4
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2021-12-30 16:53:30 +0530

    response file

--- hms/admin/response.php
+++ hms/admin/response.php
@@ -21,26 +21,10 @@ while ($row = mysqli_fetch_array($sql)) {
   $CreationDate = $row['CreationDate'];
   $UpdationDate = $row['UpdationDate'];
 
-  // $result = $ID, $PatientName, $PatientContno, $PatientGender, $CreationDate, $UpdationDate;
-
-  array_push($data, $ID, $PatientName, $PatientContno, $PatientGender, $CreationDate, $UpdationDate);
+  $result = array($ID, $PatientName, $PatientContno, $PatientGender, $CreationDate, $UpdationDate);
+  array_push($data, $result);
 }
 
-// $data = [
-//     [
-//         "Angelica",
-//         "Ramos",
-//         "System Architect",
-//         "London",
-//         "9th Oct 09",
-//         "$2,875"
-//     ],
-
-
-
-// ];
-
-//
 $results = array(
   "start" => $s,
   "lengh" => $g,
@@ -48,26 +32,5 @@ $results = array(
   "recordsFiltered" => 100,
   "data" => $data
 );
-/*while($row = $result->fetch_array(MYSQLI_ASSOC)){
-  $results["data"][] = $row ;
-}*/ //
-
 echo json_encode($results);
-
-
-
 ?>
-
-
-<!-- 
-
-
-$book =`<button type="button" data-pid="<?php //echo row['ID']; 
-                                        ?>" data-name="<?php //echo $row['PatientName']; 
-                                                                                ?>" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Book</button>`;
-
-
-										
-
-$viewdata=	`<a href="view-patient.php?viewid=<?php //echo $row['ID']; 
-                                              ?>"><i class="fa fa-eye"></i></a>`; -->
