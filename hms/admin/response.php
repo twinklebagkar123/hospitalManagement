0e4677c19a94d7040b04f8472fdb62240c1acc24
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2021-12-30 13:38:55 +0530

    Update response.php

--- hms/admin/response.php
+++ hms/admin/response.php
@@ -4,37 +4,41 @@
 
 $s=$_GET['start'];
 $g=$_GET['length'];
-$sql = mysqli_query($con, "SELECT * FROM `tblpatient` WHERE `ID` >= '$S' ORDER BY `ID` ASC LIMIT '$g'");
-										$cnt = 1;
+$sql = mysqli_query($con, "SELECT * FROM `tblpatient` WHERE `ID` >= $s ORDER BY `ID` ASC LIMIT $g");
+										
 										while ($row = mysqli_fetch_array($sql)) {
 										?>
-											<tr>
-												<td class="center"><?php echo $cnt; ?>.</td>
-												<td class="hidden-xs"><?php echo $row['PatientName']; ?></td>
-												<td><?php echo $row['PatientContno']; ?></td>
-												<td><?php echo $row['PatientGender']; ?></td>
-												<td><?php echo $row['CreationDate']; ?></td>
-												<td><?php echo $row['UpdationDate']; ?>
-												</td>
+										
+                      <?php echo $row['ID']; 
+											 echo $row['PatientName']; 
+											 echo $row['PatientContno']; 
+											 echo $row['PatientGender']; 
+												 echo $row['CreationDate']; 
+											 echo $row['UpdationDate']; ?>
+												
 
-												<td><button type="button" data-pid="<?php echo $row['ID']; ?>" data-name="<?php echo $row['PatientName']; ?>" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+												<button type="button" data-pid="<?php echo $row['ID']; ?>" data-name="<?php echo $row['PatientName']; ?>" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
 														Book
-													</button></td>
+													</button>
 
 
-												<td>
+										
 
 													<a href="view-patient.php?viewid=<?php echo $row['ID']; ?>"><i class="fa fa-eye"></i></a>
 
-												</td>
-											</tr>
+											
+										
 										<?php
-											$cnt = $cnt + 1;
-										} 
+													} 
 
 $data = [
     [
-       
+        "Angelica",
+        "Ramos",
+        "System Architect",
+        "London",
+        "9th Oct 09",
+        "$2,875"
     ],
     
    
