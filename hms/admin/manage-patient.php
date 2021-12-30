5d064ad8c6ecb0b447c44552e7296c42192d421c
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2021-12-30 16:06:41 +0530

    Update manage-patient.php

--- hms/admin/manage-patient.php
+++ hms/admin/manage-patient.php
@@ -136,7 +136,7 @@ if (isset($_POST['submit'])) {
 												<td><button type="button" data-pid="<?php echo $row['ID']; ?>" data-name="<?php echo $row['PatientName']; ?>" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
 														Book
 													</button></td>
-													<td><button type="button" data-pid="<?php echo $row['ID']; ?>" data-name="<?php echo $row['PatientName']; ?>" class="btn btn-primary" href="documents.php">
+													<td><a class="button" data-pid="<?php echo $row['ID']; ?>" data-name="<?php echo $row['PatientName']; ?>" class="btn btn-primary" href="documents.php">
 														Add
 													</button></td>
 
