eee04c8e42a24173d00b85787f36f75aa696ee87
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-01-04 12:18:01 +0530

    print demo

--- prescription_print.php
+++ prescription_print.php
@@ -1,112 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="hms/vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/print.css">
-    <title>Print</title>
-</head>
-
+<html>
 <body>
-    <!-- <header></header> -->
-    <main>
-        <button type="button" id="printDoc" style="background: pink">
-            PRINT ME!
-        </button>
-        <div id="DivIdToPrint">
-            <div class="page-header" style="text-align: center">
-                I'm The Header
-                <br />
-            </div>
 
-            <div class="page-footer">
-                I'm The Footer
-            </div>
+<h2 style="background-color: #333;">The window.print() Method</h2>
 
-            <table>
+<p>Click the button to print the current page.</p>
 
-                <thead>
-                    <tr>
-                        <td>
-                            <!--place holder for the fixed-position header-->
-                            <div class="page-header-space"></div>
-                        </td>
-                    </tr>
-                </thead>
+<button onclick="window.print()">Print this page</button>
 
-                <tbody>
-                    <tr>
-                        <td>
-                            <!--*** CONTENT GOES HERE ***-->
-                            <div class="page">PAGE 1</div>
-                            <div class="page">PAGE 2</div>
-                            <div class="page" style="line-height: 3;">
-                                PAGE 3 - Long Content
-                                <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tincidunt metus eu consectetur rutrum. Praesent tempor facilisis dapibus. Aliquam cursus diam ac vehicula pulvinar. Integer lacinia non odio et condimentum. Aenean faucibus cursus
-                                mi, sed interdum turpis sagittis a. Quisque quis pellentesque mi. Ut erat eros, posuere sed scelerisque ut, pharetra vitae tellus. Suspendisse ligula sapien, laoreet ac hendrerit sit amet, viverra vel mi. Pellentesque faucibus nisl et dolor
-                                pharetra, vel mattis massa venenatis. Integer congue condimentum nisi, sed tincidunt velit tincidunt non. Nulla sagittis sed lorem pretium aliquam. Praesent consectetur volutpat nibh, quis pulvinar est volutpat id. Cras maximus odio posuere
-                                suscipit venenatis. Donec rhoncus scelerisque metus, in tempus erat rhoncus sed. Morbi massa sapien, porttitor id urna vel, volutpat blandit velit. Cras sit amet sem eros. Quisque commodo facilisis tristique. Proin pellentesque sodales rutrum.
-                                Vestibulum purus neque, congue vel dapibus in, venenatis ut felis. Donec et ligula enim. Sed sapien sapien, tincidunt vitae lectus quis, ultricies rhoncus mi. Nunc dapibus nulla tempus nunc interdum, sed facilisis ex pellentesque. Nunc vel
-                                lorem leo. Cras pharetra sodales metus. Cras lacus ex, consequat at consequat vel, laoreet ac dui. Curabitur aliquam, sapien quis congue feugiat, nisi nisl feugiat diam, sed vehicula velit nulla ac nisl. Aliquam quis nisi euismod massa blandit
-                                pharetra nec eget nunc. Etiam eros ante, auctor sit amet quam vel, fringilla faucibus leo. Morbi a pulvinar nulla. Praesent sed vulputate nisl. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean commodo
-                                mollis iaculis. Maecenas consectetur enim vitae mollis venenatis. Ut scelerisque pretium orci id laoreet. In sit amet pharetra diam. Vestibulum in molestie lorem. Nunc gravida, eros non consequat fermentum, ex orci vestibulum orci, non accumsan
-                                sem velit ac lectus. Vivamus malesuada lacus nec velit dignissim, ac fermentum nulla pretium. Aenean mi nisi, convallis sed tempor in, porttitor eu libero. Praesent et molestie ante. Duis suscipit vitae purus sit amet aliquam. Vestibulum lectus
-                                justo, lobortis a purus a, dapibus efficitur metus. Suspendisse potenti. Duis dictum ex lorem. Suspendisse nec ligula consectetur magna hendrerit ullamcorper et eget mauris. Etiam vestibulum sodales diam, eget venenatis nunc luctus quis. Ut
-                                fermentum placerat neque nec elementum. Praesent orci erat, rhoncus vitae est eu, dictum molestie metus. Cras et fermentum elit. Aenean eget augue lacinia, varius ante in, ullamcorper dolor. Cras viverra purus non egestas consectetur. Nulla
-                                nec dolor ac lectus convallis aliquet sed a metus. Suspendisse eu imperdiet nunc, id pulvinar risus. Maecenas varius sagittis est, vel fermentum risus accumsan at. Vestibulum sollicitudin dui pharetra sapien volutpat, id convallis mi vestibulum.
-                                Phasellus commodo sit amet lorem quis imperdiet. Proin nec diam sed urna euismod ultricies at sed urna. Quisque ornare, nulla et vehicula ultrices, massa purus vehicula urna, ac sodales lacus leo vitae mi. Sed congue placerat justo at placerat.
-                                Aenean suscipit fringilla vehicula. Quisque iaculis orci vitae arcu commodo maximus. Maecenas nec nunc rutrum, cursus elit quis, porttitor sapien. Sed ac hendrerit ipsum, lacinia fringilla velit. Donec ultricies feugiat dictum.
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-
-                <tfoot>
-                    <tr>
-                        <td>
-                            <!--place holder for the fixed-position footer-->
-                            <div class="page-footer-space"></div>
-                        </td>
-                    </tr>
-                </tfoot>
-
-            </table>
-        </div>
-        <button onclick="window.print()">Print this page</button>
-    </main>
-    <script src="hms/vendor/jquery/jquery.min.js"></script>
-    <script src="hms/vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script>
-        $(document).ready(function() {
-        function printDiv() {
-
-            var divToPrint = document.getElementById('DivIdToPrint');
-
-            var newWin = window.open('', 'Print-Window');
-
-            newWin.document.open();
-
-            newWin.document.write(`
-            <html>
-            
-            <body onload="window.print()">' + divToPrint.innerHTML + '</body></html>`);
-
-            newWin.document.close();
-
-            setTimeout(function() {
-                newWin.close();
-            }, 10);
-
-        }
-    
-        $(document).on("click", "#printDoc", function() {
-            printDiv();
-        });
-    });
-    </script>
 </body>
-
 </html>
