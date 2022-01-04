383923c913e096d67dd55f1ae74ffc3dba61c284
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-01-04 16:32:48 +0530

    update

--- hms/admin/apt-response.php
+++ hms/admin/apt-response.php
@@ -12,8 +12,11 @@ $getDataFromId = $s;
 $g = $_GET['length'];
 if(isset($_SESSION['lastPageId'])): 
   $getDataFromId = $_SESSION['lastPageId'];
+  $query="SELECT tblp.ID,tblp.PatientName,doc.doctorName,doc.specilization,doc.docFees,apt.appointmentDate,apt.postingDate FROM appointment as apt INNER JOIN tblpatient AS tblp ON apt.userId = tblp.ID INNER JOIN doctors AS doc ON apt.doctorId = doc.id where tblp.ID  <= " . $getDataFromId . " ORDER BY tblp.ID DESC LIMIT ". $g;
+else:
+  $query="SELECT tblp.ID,tblp.PatientName,doc.doctorName,doc.specilization,doc.docFees,apt.appointmentDate,apt.postingDate FROM appointment as apt INNER JOIN tblpatient AS tblp ON apt.userId = tblp.ID INNER JOIN doctors AS doc ON apt.doctorId = doc.id where tblp.ID  >= " . $getDataFromId . " ORDER BY tblp.ID DESC LIMIT ". $g;
 endif;
-$query="SELECT tblp.ID,tblp.PatientName,doc.doctorName,doc.specilization,doc.docFees,apt.appointmentDate,apt.postingDate FROM appointment as apt INNER JOIN tblpatient AS tblp ON apt.userId = tblp.ID INNER JOIN doctors AS doc ON apt.doctorId = doc.id where tblp.ID  >= " . $getDataFromId . " ORDER BY tblp.ID DESC LIMIT ". $g;
+
 $appointmentCountSql ="SELECT COUNT(`id`) as totalAppointments FROM `appointment`";
 $sql = mysqli_query($con, $query);
 $countSql = mysqli_query($con, $appointmentCountSql);
