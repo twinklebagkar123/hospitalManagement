b0b4afb53aeb54c9cc814d9622962442b512524b
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-01-05 17:19:21 +0530

    doc

--- hms/admin/response.php
+++ hms/admin/response.php
@@ -32,7 +32,7 @@ while ($row = mysqli_fetch_array($sql)) {
   $CreationDate = $row['CreationDate'];
   $UpdationDate = $row['UpdationDate'];
   $bookAppointment = "<button type='button' data-pid='".$row['ID']."' data-name='".$row['PatientName']."' class='btn btn-primary' data-toggle='modal' data-target='#myModal'>Book</button>";
-  $addFiles = '<a class="btn btn-primary" data-pid="'.$row['ID'].'" data-name="'.$row['PatientName'].'" class="btn btn-primary" href="documents.php">Add</a>';
+  $addFiles = '<a class="btn btn-primary" data-pid="'.$row['ID'].'" data-name="'.$row['PatientName'].'" class="btn btn-primary" href="medical-history-documents.php">Add</a>';
   $discharge = '<a class="btn btn-primary" data-pid="'.$row['ID'].'" data-name="'.$row['PatientName'].'" class="btn btn-primary" href="discharge.php">Discharge</a>';
   $viewInfo = '<a href="view-patient.php?viewid='.$row['ID'].'"><i class="fa fa-eye"></i></a>';
   $result = array($ID, $PatientName, $PatientContno, $PatientGender, $CreationDate, $UpdationDate,$bookAppointment,$addFiles,  $discharge,$viewInfo);
