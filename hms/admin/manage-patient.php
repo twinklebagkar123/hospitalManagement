f5e37896023d3325bc32cfc728e9ee177a590675
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-01-05 14:43:48 +0530

    discharge

--- hms/admin/manage-patient.php
+++ hms/admin/manage-patient.php
@@ -113,6 +113,7 @@ if (isset($_POST['submit'])) {
 											<th>Updation Date </th>
 											 <th>Appointment </th>
 											<th>Documents </th>
+											<th>Discharge </th>
 											<th>Action</th>
 										</tr>
 									</thead>
