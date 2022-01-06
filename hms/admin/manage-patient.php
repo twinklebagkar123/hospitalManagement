cf8d98bb2229a2b206f6d99b9ed7bfeceed24ef0
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-01-06 17:34:39 +0530

    attach

--- hms/admin/manage-patient.php
+++ hms/admin/manage-patient.php
@@ -113,7 +113,7 @@ if (isset($_POST['submit'])) {
 											<th>Updation Date </th>
 											 <th>Appointment </th>
 											<th>Documents </th>
-											<th>Discharge </th>
+											
 											<th>Action</th>
 										</tr>
 									</thead>
