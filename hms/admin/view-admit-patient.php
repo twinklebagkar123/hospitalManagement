c22682477bc0cad40bce4fb5799abac3b1c57c95
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-01-06 16:12:25 +0530

    Update view-admit-patient.php

--- hms/admin/view-admit-patient.php
+++ hms/admin/view-admit-patient.php
@@ -105,7 +105,7 @@ if (isset($_POST['submit'])) {
 									<thead>
 										<tr>
 											<!-- <th class="center">#</th> -->
-											<th>test </th>
+											
 											<th>Patient Id</th>
 											<th>Doctor Name</th>
 											
