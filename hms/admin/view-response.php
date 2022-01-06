cd90b108edaebf09e1498e2b56c83da3e151fc05
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-01-06 15:25:04 +0530

    Update view-response.php

--- hms/admin/view-response.php
+++ hms/admin/view-response.php
@@ -11,12 +11,12 @@ $getDataFromId = $s;
 $g = $_GET['length'];
 if((isset($_SESSION['lastPageIdAdmit'])) && $s > 0): 
   $getDataFromId = $_SESSION['lastPageIdAdmit'];
-  $query="SELECT * FROM `patiendAdmission` WHERE `ID` <= " . $getDataFromId . " ORDER BY `ID` ASC LIMIT " . $g;
+  $query="SELECT * FROM `patientAdmission` WHERE `unqId` <= " . $getDataFromId . " ORDER BY `unqId` ASC LIMIT " . $g;
 else:
-  $query="SELECT * FROM `patiendAdmission` WHERE `ID` >= " . $getDataFromId . " ORDER BY `ID` ASC LIMIT " . $g;
+  $query="SELECT * FROM `patientAdmission` WHERE `unqId` >= " . $getDataFromId . " ORDER BY `unqId` ASC LIMIT " . $g;
 endif;
 
-$patientCountSql ="SELECT COUNT(`unqId`) FROM `patiendAdmission`";
+$patientCountSql ="SELECT COUNT(`unqId`) FROM `patientAdmission`";
 $sql = mysqli_query($con, $query);
 $countSql = mysqli_query($con, $patientCountSql);
 $resultOfAPatientCount = mysqli_fetch_array($countSql);
@@ -26,16 +26,16 @@ while ($row = mysqli_fetch_array($sql)) {
   $_SESSION['lastPageIdAdmit'] = $row['unqId'];
 
   $ID = $row['uId'];
-  $PatientName =   $row['docId'];
-  $PatientContno = $row['wardNo'];
-  $PatientGender = $row['dateofadmission'];
-  $CreationDate = $row['dateofdischarg'];
-  $UpdationDate = $row['advance_paid'];
-  $bookAppointment = "<button type='button' data-pid='".$row['ID']."' data-name='".$row['PatientName']."' class='btn btn-primary' data-toggle='modal' data-target='#myModal'>Operation</button>";
-  $addFiles = '<a class="btn btn-primary" data-pid="'.$row['ID'].'" data-name="'.$row['PatientName'].'" class="btn btn-primary" href="medical-history-documents.php">Add</a>';
+  $doc =   $row['docId'];
+  $ward = $row['wardNo'];
+  $admission = $row['dateofadmission'];
+  $discharge = $row['dateofdischarg'];
+  $advanve = $row['advance_paid'];
+  $operation = "<button type='button' data-pid='".$row['ID']."' data-name='".$row['PatientName']."' class='btn btn-primary' data-toggle='modal' data-target='#myModal'>Operation</button>";
+  $id = '<a class="btn btn-primary" data-pid="'.$row['ID'].'" data-name="'.$row['PatientName'].'" class="btn btn-primary" href="medical-history-documents.php">id</a>';
   $discharge = '<a class="btn btn-primary"  class="btn btn-primary" href="discharge.php">Discharge</a>';
-   $viewInfo = '<a href="view-patient.php?viewid='.$row['ID'].'"><i class="fa fa-eye"></i></a>';
-  $result = array($ID, $PatientName, $PatientContno, $PatientGender, $CreationDate, $UpdationDate,$bookAppointment,$addFiles,  $discharge,$viewInfo);
+   
+  $result = array($ID, $doc, $ward, $admission, $discharge, $advanve,$operation,$id,  $discharge,$discharge);
   array_push($data, $result);
 }
 
