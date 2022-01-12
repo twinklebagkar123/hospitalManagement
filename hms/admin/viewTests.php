854849652104fb80f381ad648cf4b8f50a4a4de4
twinklebagkar123 <twinklebagkar99@gmail.com>
2022-01-12 16:39:58 +0530

    show tests

--- hms/admin/viewTests.php
+++ hms/admin/viewTests.php
@@ -53,33 +53,40 @@ check_login();
                     <div class="container-fluid container-fullw bg-white">
                         <div class="row">
                             <div class="col-sm-12">
-                                <!-- Tabs navs -->
-                                <ul class="nav nav-tabs nav-fill mb-3" id="ex1" role="tablist">
-                                    <li class="nav-item" role="presentation">
-                                        <a class="nav-link active" id="ex2-tab-1" data-mdb-toggle="tab" href="#ex2-tabs-1" role="tab" aria-controls="ex2-tabs-1" aria-selected="true">Link</a>
-                                    </li>
-                                    <li class="nav-item" role="presentation">
-                                        <a class="nav-link" id="ex2-tab-2" data-mdb-toggle="tab" href="#ex2-tabs-2" role="tab" aria-controls="ex2-tabs-2" aria-selected="false">Very very very very long link</a>
-                                    </li>
-                                    <li class="nav-item" role="presentation">
-                                        <a class="nav-link" id="ex2-tab-3" data-mdb-toggle="tab" href="#ex2-tabs-3" role="tab" aria-controls="ex2-tabs-3" aria-selected="false">Another link</a>
-                                    </li>
-                                </ul>
-                                <!-- Tabs navs -->
-
-                                <!-- Tabs content -->
-                                <div class="tab-content" id="ex2-content">
-                                    <div class="tab-pane fade show active" id="ex2-tabs-1" role="tabpanel" aria-labelledby="ex2-tab-1">
-                                        Tab 1 content
-                                    </div>
-                                    <div class="tab-pane fade" id="ex2-tabs-2" role="tabpanel" aria-labelledby="ex2-tab-2">
-                                        Tab 2 content
-                                    </div>
-                                    <div class="tab-pane fade" id="ex2-tabs-3" role="tabpanel" aria-labelledby="ex2-tab-3">
-                                        Tab 3 content
-                                    </div>
-                                </div>
-                                <!-- Tabs content -->
+                            <?php
+									$admissionQuery = "SELECT * FROM `laboratoryTestList` where 1";
+									$result = $con->query($admissionQuery);
+									?>
+									<table class="table table-bordered dt-responsive nowrap">
+										<thead>
+											<th>#</th>
+											<th>Assigned Date</th>
+											<th>Test Type</th>
+											<th>Patient Name</th>
+											<th>Status</th>
+											<th>Perform Test</th>
+											<th>Reports</th>
+										</thead>
+										<tbody id="viewReport">
+											<?php
+											$sr = 1;
+											while ($row = mysqli_fetch_array($result)) {
+											?>
+												<tr>
+													<td><?php echo $sr; ?></td>
+													<td id="date"><?php echo $row['assignedDate']; ?></td>
+													<td><?php echo $row['performedTestID']; ?></td>
+													<td><?php ?></td>
+													<td><?php echo $row['status']; ?></td>
+													<td><button type="button" >Perform Test</button></td>
+													<td></td>
+												</tr>
+											<?php
+												$sr++;
+											}
+											?>
+										</tbody>
+									</table>
                             </div>
                         </div>
                     </div>
