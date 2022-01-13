95b68fd6cfb8a78d5428de6d64003d2feae35414
twinklebagkar123 <twinklebagkar99@gmail.com>
2022-01-13 12:55:03 +0530

    update json

--- hms/admin/performTest.php
+++ hms/admin/performTest.php
@@ -26,10 +26,10 @@ if (isset($_POST['submit'])) {
 
     }
     $date = date("Y-m-d");
-    echo json_encode($testresult);
-    //$updateQuery = "UPDATE `labTestRecord` SET `labTestStatus`='complete',`testResult`='$testresult',`performedDate`='$date',`performedBy`='test',`charges`='$charges' WHERE recordID = '$recID'";
+    $testJson = json_encode($testresult);
+    $updateQuery = "UPDATE `labTestRecord` SET `labTestStatus`='complete',`testResult`='$testJson',`performedDate`='$date',`performedBy`='test',`charges`='$charges' WHERE recordID = '$recID'";
     //print_r($updateQuery);
-   // $result = $con->query($updateQuery);
+    $result = $con->query($updateQuery);
 }
 
 ?>
