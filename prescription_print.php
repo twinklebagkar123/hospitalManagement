21daa92f4215061a2c24135156bff985d5e0f06d
twinklebagkar123 <twinklebagkar99@gmail.com>
2022-01-20 15:35:08 +0530

    update page

--- prescription_print.php
+++ prescription_print.php
@@ -1,12 +1,40 @@
 <!DOCTYPE html>
 <html>
+
+<head>
+    <title>SAHRC | PRESCRIPTION</title>
+
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+
+</head>
+
 <body>
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                        <h3> St. Anthony Hostpital & Research Center </h3>
+                </div>
 
-<h2 style="background-color: #333;">The window.print() Method</h2>
+            </div>
+        </div>
+    </section>
 
-<p>Click the button to print the current page.</p>
+    <h2 style="background-color: #333;">The window.print() Method</h2>
 
-<button onclick="window.print()">Print this page</button>
+    <p>Click the button to print the current page.</p>
+
+    <button onclick="window.print()">Print this page</button>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </body>
+
 </html>
