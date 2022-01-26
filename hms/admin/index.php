de4445953912d800493f0a733c0c7992858666bc
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-01-26 15:40:11 +0530

    update

--- hms/admin/index.php
+++ hms/admin/index.php
@@ -27,24 +27,28 @@
 	// error_reporting(0);
 	include("include/config.php");
 	if (isset($_POST['submit'])) {
-		$ret = mysqli_query($con, "SELECT * FROM admin WHERE username='" . $_POST['username'] . "' and password='" . $_POST['password'] . "'");
-		// echo $ret . " USERNAME: " . $_POST['username'] . " PASSWORD: " . $_POST['password'] . "";
-		$num = mysqli_fetch_array($ret);
-		if ($num > 0) {
-			$extra = "dashboard.php"; //
-			$_SESSION['login'] = $_POST['username'];
-			$_SESSION['id'] = $num['id'];
-			$host = $_SERVER['HTTP_HOST'];
-			$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-			header("location:http://$host$uri/$extra");
-			exit();
-		} else {
-			$_SESSION['errmsg'] = "Invalid username or password";
-			$extra = "index.php";
-			$host  = $_SERVER['HTTP_HOST'];
-			$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-			header("location:http://$host$uri/$extra");
-			exit();
+		try {
+			$ret = mysqli_query($con, "SELECT * FROM admin WHERE username='" . $_POST['username'] . "' and password='" . $_POST['password'] . "'");
+			// echo $ret . " USERNAME: " . $_POST['username'] . " PASSWORD: " . $_POST['password'] . "";
+			$num = mysqli_fetch_array($ret);
+			if ($num > 0) {
+				$extra = "dashboard.php"; //
+				$_SESSION['login'] = $_POST['username'];
+				$_SESSION['id'] = $num['id'];
+				$host = $_SERVER['HTTP_HOST'];
+				$uri = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+				header("location:http://$host$uri/$extra");
+				exit();
+			} else {
+				$_SESSION['errmsg'] = "Invalid username or password";
+				$extra = "index.php";
+				$host  = $_SERVER['HTTP_HOST'];
+				$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+				header("location:http://$host$uri/$extra");
+				exit();
+			}
+		} catch (\Throwable $th) {
+			print_r($th);
 		}
 	}
 	?>
