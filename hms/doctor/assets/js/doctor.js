cbc760a7ffae84021d85b322076efdd494b1928d
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-01-28 15:39:46 +0530

    update

--- hms/doctor/assets/js/doctor.js
+++ hms/doctor/assets/js/doctor.js
@@ -1,24 +1,26 @@
 $(document).ready(function(){
     $(document).on("click","#pillResult span", function(){
+        
         console.log("hello");
         var name = $(this).data("name");
-        var $edit = $("#result");
-        var curValue =  $("#result").val();
-        console.log(curValue);
-        console.log("name:"+name);
-        $('#medicalResult').append(`<div class="chip">
-        <span class="medicineName">${name}</span>
-        <button type="button" class="zap">X</button>
-        </div>`);
-        if(curValue.length > 0 ){
-            var newValue = curValue+","+name;
-            console.log("in loop:" +newValue);
-        }
-        else{
-            var newValue = name;
-        }
-        console.log("newValue: "+newValue);
-        $edit.val(newValue);
+        $('#autosuggest').val(name);
+        // var $edit = $("#result");
+        // var curValue =  $("#result").val();
+        // console.log(curValue);
+        // console.log("name:"+name);
+        // $('#medicalResult').append(`<div class="chip">
+        // <span class="medicineName">${name}</span>
+        // <button type="button" class="zap">X</button>
+        // </div>`);
+        // if(curValue.length > 0 ){
+        //     var newValue = curValue+","+name;
+        //     console.log("in loop:" +newValue);
+        // }
+        // else{
+        //     var newValue = name;
+        // }
+        // console.log("newValue: "+newValue);
+        // $edit.val(newValue);
         // $("#result").val().append(", ");
       });
     $(document).on("click", ".zap" , function() {
@@ -27,21 +29,15 @@ $(document).ready(function(){
             console.log("Chip On Click Value: ",searchInArray)
             let arrayFromRes = $("#result").val().split(','); 
             var index = arrayFromRes.indexOf(searchInArray);
-        
             console.log( "print array",arrayFromRes,index);
             if (index > -1) {
-
             arrayFromRes.splice(index, 1);
             console.log( " in splice",arrayFromRes);
             }
-            // else{
-            //     arrayFromRes = [];
-            // }
             var stringvar = arrayFromRes.join(",").toString()
             console.log(stringvar+" string value");
             $("#result").val(stringvar);
 	    	$(this).remove();
-           
 	    });
 	});
 });
