f2f2cc1d02424c3d350406e3e95c7af9a46ca2fd
twinklebagkar123 <twinklebagkar99@gmail.com>
2022-01-31 16:13:53 +0530

    lab result

--- hms/admin/testResultReport.php
+++ hms/admin/testResultReport.php
@@ -34,7 +34,6 @@ function fetchPatientName($admissionID)
     $result =  $con->query($query);
     while ($row = mysqli_fetch_array($result)) {
         $answer = $row['PatientName'];
-      
     }
     return $answer;
 }
@@ -44,11 +43,70 @@ function fetchTestName($testID)
     $query = "SELECT * FROM `laboratoryTestList` where labFormID= '$testID'";
     $result = $con->query($query);
     while ($row = mysqli_fetch_array($result)) {
-      $answer = $row['labTestName'];
+        $answer = $row['labTestName'];
     }
     return $answer;
 }
 ?>
+<style>
+    .resultSection header {
+        min-height: 83px;
+        border-bottom: 1px solid black;
+
+    }
+
+    .doc-details {
+        margin-top: 5px;
+        margin-left: 15px;
+
+    }
+
+    .clinic-details {
+        margin-top: 5px;
+        margin-left: 15px;
+    }
+
+    .doc-name {
+        font-weight: bold;
+        margin-bottom: 5px;
+
+    }
+
+    .doc-meta {
+        font-size: 10px;
+    }
+
+    .datetime {
+        font-size: 12px;
+        margin-top: 5px;
+
+    }
+
+    .row.title {
+        font-weight: bold;
+        padding-left: 10px;
+        margin-top: 10px;
+        margin-bottom: 10px;
+    }
+
+    .prescription {
+        min-height: 380px;
+        margin-bottom: 10px;
+    }
+
+    .border {
+        border: 1px solid black;
+    }
+
+    .reportHeader {
+        border: 1px solid black;
+    }
+
+    .instruction {
+        font-size: 12px;
+    }
+</style>
+
 <body>
     <div id="app">
         <?php include('include/sidebar.php'); ?>
@@ -72,52 +130,76 @@ function fetchTestName($testID)
                             </ol>
                         </div>
                     </section>
-                    <div class="container-fluid container-fullw bg-white">
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <?php
-                                   $recID = $_GET['recID'];
-                                   $queryRec = "SELECT * FROM `labTestRecord` where recordID='$recID'";
-                                   $result = $con->query($queryRec);
-                                   while ($row = mysqli_fetch_array($result)) {
-                                    ?>
-                                      <h3>patient name: <?php echo fetchPatientName($row['admissionID']);?></h3>
-                                      <h3>Test Name: <?php echo fetchTestName($row['performedTestID']);?></h3>
-                                      <h3>Test Assigned Date: <?php echo $row['assignedDate'];?></h3>
-                                      <h3>Test Performed Date: <?php echo $row['performedDate'];?></h3>
-                                      <p>Lab Test Findings: </p>
-                                    <?php
-                                   $recResult=  json_decode($row['testResult']);
-                                     foreach ($recResult as $key => $value) {
-                                         ?>
-                                         <p> <?php echo $key; ?> : <?php echo $value;?></p>
-                                         <?php
-                                     }
-                                     ?>
-                                     <h3>Report Status: <?php echo $row['labTestStatus'];?></h3>
-                                     <h3>Report Remarks: <?php echo $row['remark'];?></h3>
-                                     <h3>Laboratory/Test Incharge: <?php echo $row['performedBy'];?></h3>
-                                     <h3>Report Summary: <?php echo $row['findings'];?></h3>
-                                     <?php
-                                   }
+                    <section class="resultSection">
+                        <div class="container border">
+                            <header class="row">
+                                <div class="col-md-10">
+                                    <div class="doc-details">
+                                        <p class="doc-name">St. Anthony's Hospital & Research Center</p>
+                                        <p class="doc-meta">General Hospital</p>
+                                        <p class="doc-meta">Rgn: 2341</p>
+                                    </div>
 
-                                ?>
+                                    <div class="clinic-details">
+                                        <p class="doc-meta">Doctor Name: </p>
+                                        <p class="doc-meta">Anjana Gupta</p>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-2 datetime">
+                                    <p>Date: <?php echo date("Y/m/d"); ?></p>
+                                    <p>Time: <?php echo date("h:i:sa"); ?></p>
+                                </div>
+                            </header>
+                        </div>
+                        <div class="container-fluid container-fullw bg-white">
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <?php
+                                    $recID = $_GET['recID'];
+                                    $queryRec = "SELECT * FROM `labTestRecord` where recordID='$recID'";
+                                    $result = $con->query($queryRec);
+                                    while ($row = mysqli_fetch_array($result)) {
+                                    ?>
+                                        <h3>patient name: <?php echo fetchPatientName($row['admissionID']); ?></h3>
+                                        <h3>Test Name: <?php echo fetchTestName($row['performedTestID']); ?></h3>
+                                        <h3>Test Assigned Date: <?php echo $row['assignedDate']; ?></h3>
+                                        <h3>Test Performed Date: <?php echo $row['performedDate']; ?></h3>
+                                        <p>Lab Test Findings: </p>
+                                        <?php
+                                        $recResult =  json_decode($row['testResult']);
+                                        foreach ($recResult as $key => $value) {
+                                        ?>
+                                            <p> <?php echo $key; ?> : <?php echo $value; ?></p>
+                                        <?php
+                                        }
+                                        ?>
+                                        <h3>Report Status: <?php echo $row['labTestStatus']; ?></h3>
+                                        <h3>Report Remarks: <?php echo $row['remark']; ?></h3>
+                                        <h3>Laboratory/Test Incharge: <?php echo $row['performedBy']; ?></h3>
+                                        <h3>Report Summary: <?php echo $row['findings']; ?></h3>
+                                    <?php
+                                    }
+
+                                    ?>
+                                </div>
                             </div>
                         </div>
-                    </div> 
+                    </section>
+
                 </div>
             </div>
         </div>
     </div>
-<!-- start: FOOTER -->
-<?php include('include/footer.php'); ?>
+    <!-- start: FOOTER -->
+    <?php include('include/footer.php'); ?>
     <!-- end: FOOTER -->
 
     <!-- start: SETTINGS -->
     <?php include('include/setting.php'); ?>
 
     <!-- end: SETTINGS -->
-    </div> 
+    </div>
     <!-- start: MAIN JAVASCRIPTS -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
