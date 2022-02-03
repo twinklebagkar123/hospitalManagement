f46f19ae57a3608c0ecca6a27c3ee78c0326d81a
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-02-03 11:59:23 +0530

    update

--- hms/doctor/view-patient.php
+++ hms/doctor/view-patient.php
@@ -290,7 +290,7 @@ if (isset($_POST['submit'])) {
 
                     </div>
                   </div>
-                  <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal fade" id="myModal" tabindex="1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div class="modal-dialog modal-lg" role="document">
                       <div class="modal-content">
                         <div class="modal-header">
