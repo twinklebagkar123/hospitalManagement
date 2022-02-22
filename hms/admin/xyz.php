c07f98167c67736fdb54455408260ee571a3d4c9
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-02-22 10:42:03 +0530

    Update xyz.php

--- hms/admin/xyz.php
+++ hms/admin/xyz.php
@@ -4,7 +4,7 @@ error_reporting(0);
 include('include/config.php');
 include('include/checklogin.php');
 check_login();
-if(isset($_POST['submit']))
+if(isset($_POST['']))
 {
 	$tclassname=$_POST['tariff_class_name'];
 	
@@ -101,79 +101,62 @@ if(isset($_POST['submit']))
 													</button>
 												</form>
 											</div>
-                                            <div class="row">
-									<div class="col-md-12">
-										<h5 class="over-title margin-bottom-15">Manage <span class="text-bold">Ambulance</span></h5>
-										<p style="color:red;"><?php echo htmlentities($_SESSION['msg']); ?>
-											<?php echo htmlentities($_SESSION['msg'] = ""); ?></p>
-										<table class="table table-hover" id="sample-table-1">
-											<thead>
-												<tr>
-													<th class="center">#</th>
-													<th>name</th>
-													<th>Price</th>
-													<th>fee distributed</th>
+                                            <?php
+if(isset($_POST['tariff_cat_name']))
+{ 
 
-													<th>Action</th>
+$sdata=$_POST['searchdata'];
+  ?>
+  <h4 align="center">Result against "<?php echo $sdata;?>" keyword </h4>
+<table class="table table-hover" id="sample-table-1">
+<thead>
+<tr>
 
-												</tr>
-											</thead>
-											<tbody id="delete">
-												<?php
-												$sql = mysqli_query($con, "SELECT * FROM `tariff_room_info`");
-												$cnt = 1;
-												while ($row = mysqli_fetch_array($sql)) {
-												?>
+<th>name</th>
+<th>Fee distribution</th>
+<th>total price</th>
 
-													<tr>
-														<td class="center"><?php echo $cnt; ?>.</td>
-														<td><?php echo $row['tariff_room_name']; ?></td>
-														<td><?php echo $row['tariff_room_fee']; ?></td>
-														<td><?php echo $row['tariff_fee_distribution']; ?></td>
+<th>Action</th>
+</tr>
+</thead>
+<tbody>
+<?php
 
-														<td>
-															<div class="visible-md visible-lg hidden-sm hidden-xs">
-																<a href="editAmbulance.php?id=<?php echo $row['id']; ?>" class="btn btn-transparent btn-xs" tooltip-placement="top" tooltip="Edit"><i class="fa fa-pencil"></i></a>
-																<!-- add-medicine.php?code=<?php //echo $row['code']
-																							?>&del=delete										 -->
-																<a href="#" class="btn btn-transparent btn-xs tooltips dellClass " data-id='<?php echo $row['id']; ?>' id="del" tooltip-placement="top" tooltip="Remove"><i class="fa fa-times fa fa-white"></i></a>
-															</div>
-															<div class="visible-xs visible-sm hidden-md hidden-lg">
-																<div class="btn-group" dropdown is-open="status.isopen">
-																	<button type="button" class="btn btn-primary btn-o btn-sm dropdown-toggle" dropdown-toggle>
-																		<i class="fa fa-cog"></i>&nbsp;<span class="caret"></span>
-																	</button>
-																	<ul class="dropdown-menu pull-right dropdown-light" role="menu">
-																		<li>
-																			<a href="#">
-																				Edit
-																			</a>
-																		</li>
-																		<li>
-																			<a href="#">
-																				Share
-																			</a>
-																		</li>
-																		<li>
-																			<a href="#">
-																				Remove
-																			</a>
-																		</li>
-																	</ul>
-																</div>
-															</div>
-														</td>
-													</tr>
+$sql=mysqli_query($con,"select * from tariff_room_info where tariff_cat_id like '%$sdata%'");
+$num=mysqli_num_rows($sql);
+if($num>0){
+$cnt=1;
+while($row=mysqli_fetch_array($sql))
+{
+?>
+<tr>
 
-												<?php
-													$cnt = $cnt + 1;
-												} ?>
+<td class="hidden-xs"> <?php echo $row['ID'];?> </td>
+	
 
+<td><?php echo $row['tariff_room_name'];?></td>
+<td><?php echo $row['is_fee_distributed'];?></td>
+<td><?php echo $row['tariff_room_fee'];?></td>
 
-											</tbody>
-										</table>
-									</div>
-								</div>
+<td><?php echo $row['UpdationDate'];?>
+</td>
+<td>
+
+<a href="view-patient.php?viewid=<?php echo $row['ID'];?>"><i class="fa fa-eye"></i></a>
+
+</td>
+</tr>
+<?php 
+$cnt=$cnt+1;
+} } else { ?>
+  <tr>
+    <td colspan="8"> No record found against this search</td>
+
+  </tr>
+   
+<?php } }?></tbody>
+</table>
+
 										</div>
 									</div>
 
