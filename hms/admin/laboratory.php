7deafa99eec7a3a97286ea1cf1eae10db96c5a94
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-03-02 17:48:15 +0530

    ckedotr

--- hms/admin/laboratory.php
+++ hms/admin/laboratory.php
@@ -246,7 +246,7 @@ if(isset($_POST['submit']))
 	<!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
 	<!-- start: CLIP-TWO JAVASCRIPTS -->
 	<script src="assets/js/main.js"></script>
-	<script src="assets/js/ckeditor.js"></script>
+	<script src="assets/ckeditor/ckeditor.js"></script>
 	 
 	<!-- start: JavaScript Event Handlers for this page -->
 	<script src="assets/js/form-elements.js"></script>
