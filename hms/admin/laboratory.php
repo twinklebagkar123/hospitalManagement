7093675675cdae30d5cc21aba17319e62f517e06
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-03-03 17:51:20 +0530

    test

--- hms/admin/laboratory.php
+++ hms/admin/laboratory.php
@@ -12,7 +12,7 @@ if(isset($_POST['submit']))
 $html_test_default_info=htmlentities($_POST['html_test_default_info'], ENT_QUOTES) ;
 
 
-	$query = "INSERT INTO `laboratoryTestList`(`labTestName`, `labFields`, `labCharges`,`test_more_info`) VALUES ('$labTestName','$labFields','$charges','$html_test_default_info')";
+	$query = "INSERT INTO `laboratoryTestList`(`labTestName`, `labFields`, `labCharges`,`test_more_info`) VALUES ('$labTestName','$labFields','$charges','$html_test_default_info' )";
 	//$con->query($query);
 	print_r($query);
 	$stat = true;
