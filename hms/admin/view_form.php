6a790701ad799acb46102b733b638756a894d4d4
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-03-14 16:33:57 +0530

    rev arr

--- hms/admin/view_form.php
+++ hms/admin/view_form.php
@@ -110,6 +110,7 @@ $id= $_GET['id'];
                                         $titles = $row['main_titles'];
 
                                         $title_arr = explode(",", $fields);
+										print_r(array_reverse($fields));
                                         $i=1;
                                         foreach ($title_arr as  $titles) {
 
