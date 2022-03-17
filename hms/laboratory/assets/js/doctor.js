c7998f56522f73b624cd4bd4c5647abc107b6939
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-03-17 15:39:47 +0530

    laboratory section

---
+++ hms/laboratory/assets/js/doctor.js
@@ -0,0 +1,114 @@
+$(document).ready(function(){
+    var medList = [];
+    
+    //from above getting index we can remove data when required.
+    //also json stringify to store data in input type hidden and json decode in php 
+    $(document).on("click","#remove_medicine", function(){
+        var medRow = $(this).parent().parent().closest('tr').find('td').first().text();
+        var medIndex = medList.map(function(e) { return e.medicineName; }).indexOf(medRow);
+        medList.splice(medIndex, 1);
+        
+        $('#medicinePrescription').val(JSON.stringify(medList));
+        $(this).parent().parent().closest('tr').remove();
+    });
+    $(document).on("click","#general_prescription", function(){
+        $('.general_prescription').css("display","block");
+        $('#medicinePrescriptionType').val("general_prescription");
+        $('.hourly_prescription').css("display","none");
+    });
+    $(document).on("click","#hourly_prescription", function(){
+        $('.general_prescription').css("display","none");
+        $('#medicinePrescriptionType').val("hourly_prescription");
+        $('.hourly_prescription').css("display","block");
+    });
+    $(document).on("click",".addMedicineBtn", function(){
+        
+        var prescription_type = $('#medicinePrescriptionType').val();
+        if(prescription_type == "hourly_prescription"){
+            var medicineName = $('.hourly_prescription .autosuggest').val();
+            var start_from = $('#start_from').val();
+            var dosage = $('#dosage_hourly').val();
+            var interval_hourly = parseInt($('#interval_hourly').val());
+            medList.push({medicineName: medicineName,start_from: start_from,dosage: dosage,interval_hourly: interval_hourly,prescription_type:prescription_type});
+            $('#medicinePrescription').val(JSON.stringify(medList));
+            $('#prescribedMedicineList').css("display","none"); 
+            $('#prescribedMedicineListHourly').css("display","block"); 
+            $('.medicineList').append(`<tr>
+                <td>${medicineName}</td>
+                <td>${start_from}</td>
+                <td>${dosage}</td>
+                <td><div class="row"><div class="col-md-6">${interval_hourly}</div><div class="col-md-6 text-right"><button  id="remove_medicine" class=" btn btn-primary" style="
+                padding: 0px 10px;
+            ">-</button></div></div>
+                </td>
+                </tr>`);
+        }
+        else{
+            var medicineName = $('.general_prescription .autosuggest').val();
+            var frequency = $('#frequency').val();
+            var dosage = $('#dosage').val();
+            var period = $('#period').val();
+            var mealCheck = $('#meal_check');
+            var mealDetail = "";
+            if(mealCheck.is(':checked')){
+                mealDetail = "Before Meal";
+            }else{
+                mealDetail = "After Meal";
+            }
+            medList.push({medicineName: medicineName,frequency: frequency,dosage: dosage,period: period,mealDetail:mealDetail,prescription_type:prescription_type});
+            $('#medicinePrescription').val(JSON.stringify(medList));
+            $('#prescribedMedicineList').css("display","block"); 
+            $('#prescribedMedicineListHourly').css("display","none"); 
+            $('.medicineList').append(`<tr>
+                <td>${medicineName}</td>
+                <td>${frequency}</td>
+                <td>${dosage}</td>
+                <td><div class="row"><div class="col-md-6">${period}</div><div class="col-md-6 text-right"><button  id="remove_medicine" class=" btn btn-primary" style="
+                padding: 0px 10px;
+            ">-</button></div></div>
+                </td>
+                <td>${mealDetail}</td>
+                </tr>`);
+        }
+    });
+    
+    $(document).on("click",".pillResult span", function(){
+        var name = $(this).data("name");
+        $('.autosuggest').val(name);
+        $('.pillResult').empty();
+        
+        // var $edit = $("#result");
+        // var curValue =  $("#result").val();
+        // console.log(curValue);
+        // console.log("name:"+name);
+        
+        // if(curValue.length > 0 ){
+        //     var newValue = curValue+","+name;
+        //     console.log("in loop:" +newValue);
+        // }
+        // else{
+        //     var newValue = name;
+        // }
+        // console.log("newValue: "+newValue);
+        // $edit.val(newValue);
+        // $("#result").val().append(", ");
+      });
+    
+    $(document).on("click", ".zap" , function() {
+	    $(this).closest('.chip').fadeOut( 500, function() {
+            var searchInArray = $(this).children('.medicineName').text();
+            console.log("Chip On Click Value: ",searchInArray)
+            let arrayFromRes = $("#result").val().split(','); 
+            var index = arrayFromRes.indexOf(searchInArray);
+            console.log( "print array",arrayFromRes,index);
+            if (index > -1) {
+            arrayFromRes.splice(index, 1);
+            console.log( " in splice",arrayFromRes);
+            }
+            var stringvar = arrayFromRes.join(",").toString()
+            console.log(stringvar+" string value");
+            $("#result").val(stringvar);
+	    	$(this).remove();
+	    });
+	});
+});
---
+++ hms/laboratory/include/config.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_SERVER','localhost');
+define('DB_USER','u298126064_hospital');
+define('DB_PASS' ,'?mnR!Y!y0');
+define('DB_NAME', 'u298126064_hospital');
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+// Check connection
+if (mysqli_connect_errno())
+{
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+?>
