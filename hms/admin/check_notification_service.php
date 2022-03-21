2a2b9a08212d845af5f4e2108c9745b33026ad1c
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-03-21 12:51:18 +0530

    update

--- hms/admin/check_notification_service.php
+++ hms/admin/check_notification_service.php
@@ -5,5 +5,5 @@ include('include/checklogin.php');
 check_login();
 $sql = "SELECT * FROM `notification_detail` WHERE `notification_type` = 'admin' AND `read_receipt` = 0";
 $result = mysqli_query($con, $sql);
-echo json_encode(mysqli_fetch_array($result));
+echo json_encode($result -> fetch_array(MYSQLI_ASSOC));
 ?>
