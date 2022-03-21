6513c10bdddf5537bb8ecdbdf9a16421311634a6
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-03-21 12:45:14 +0530

    update query error

--- hms/admin/check_notification_service.php
+++ hms/admin/check_notification_service.php
@@ -4,6 +4,6 @@ include('include/config.php');
 include('include/checklogin.php');
 check_login();
 $sql = "SELECT * FROM `notification_detail` WHERE `notification_type` = 'admin' AND `read_receipt` = 0";
-$result = mysqli_query($con, $query);
+$result = mysqli_query($con, $sql);
 echo json_encode(mysqli_fetch_array($result));
 ?>
