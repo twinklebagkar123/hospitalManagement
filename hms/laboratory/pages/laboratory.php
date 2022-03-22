c94ed5fbfd2d85b8b8c1f02c3c3b211e2b337ed5
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-03-22 15:44:57 +0530

    Update laboratory.php

--- hms/laboratory/pages/laboratory.php
+++ hms/laboratory/pages/laboratory.php
@@ -32,21 +32,7 @@ $html_test_default_info=htmlentities($_POST['html_test_default_info'], ENT_QUOTE
 
 <head>
 	<title>Admin | Laboratory Section</title>
-
-	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
-	<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
-	<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
-	<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
-	<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
-	<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
-	<link href="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
-	<link href="vendor/select2/select2.min.css" rel="stylesheet" media="screen">
-	<link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
-	<link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
-	<link rel="stylesheet" href="assets/css/styles.css">
-	<link rel="stylesheet" href="assets/css/plugins.css">
-	<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+	<?php include('include/header.php') ?>
 	<script type="text/javascript">
 		function valid() {
 			if (document.adddoc.npass.value != document.adddoc.cfpass.value) {
@@ -116,9 +102,10 @@ $html_test_default_info=htmlentities($_POST['html_test_default_info'], ENT_QUOTE
 </style>
 	<div id="app">
 		<?php include('include/sidebar.php'); ?>
+		
 		<div class="app-content">
 
-			<?php include('include/header.php'); ?>
+			
 
 			<!-- end: TOP NAVBAR -->
 			<div class="main-content">
