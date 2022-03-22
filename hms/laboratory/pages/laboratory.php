b4b454436a5d252aaf88cb53e245663a58b457c1
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-03-22 15:39:46 +0530

    Update laboratory.php

--- hms/laboratory/pages/laboratory.php
+++ hms/laboratory/pages/laboratory.php
@@ -3,7 +3,7 @@ session_start();
 // error_reporting(0);
 include('include/config.php');
 include('include/checklogin.php');
- include('include/header.php') ;
+
 check_login();
 if(isset($_POST['submit']))
 {
@@ -33,6 +33,20 @@ $html_test_default_info=htmlentities($_POST['html_test_default_info'], ENT_QUOTE
 <head>
 	<title>Admin | Laboratory Section</title>
 
+	<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+	<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+	<link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+	<link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+	<link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+	<link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+	<link href="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
+	<link href="vendor/select2/select2.min.css" rel="stylesheet" media="screen">
+	<link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
+	<link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
+	<link rel="stylesheet" href="assets/css/styles.css">
+	<link rel="stylesheet" href="assets/css/plugins.css">
+	<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
 	<script type="text/javascript">
 		function valid() {
 			if (document.adddoc.npass.value != document.adddoc.cfpass.value) {
@@ -377,42 +391,9 @@ print_r($th);
 	</div>
 	</div>
 	<!-- start: FOOTER -->
-	<?php include('include/footer.php'); ?>
-	<!-- end: FOOTER -->
-
-	<!-- start: SETTINGS -->
-	<?php include('include/setting.php'); ?>
-
-	<!-- end: SETTINGS -->
-	</div>
-	<!-- start: MAIN JAVASCRIPTS -->
-	<script src="vendor/jquery/jquery.min.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-	<script src="vendor/modernizr/modernizr.js"></script>
-	<script src="vendor/jquery-cookie/jquery.cookie.js"></script>
-	<script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-	<script src="vendor/switchery/switchery.min.js"></script>
-	
-
-	<!-- end: MAIN JAVASCRIPTS -->
-	<!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
-	<script src="vendor/maskedinput/jquery.maskedinput.min.js"></script>
-	<script src="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
-	<script src="vendor/autosize/autosize.min.js"></script>
-	<script src="vendor/selectFx/classie.js"></script>
-	<script src="vendor/selectFx/selectFx.js"></script>
-	<script src="vendor/select2/select2.min.js"></script>
-	<script src="vendor/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
-	<script src="vendor/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
-	<!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
-	<!-- start: CLIP-TWO JAVASCRIPTS -->
-	<script src="assets/js/main.js"></script>
-	<script src="assets/ckeditor/ckeditor.js"></script>
-	 
-	<!-- start: JavaScript Event Handlers for this page -->
-	<script src="assets/js/form-elements.js"></script>
-	<script type="text/javascript">
-    function blockSpecialChar(e){
+	<?php include('include/header.php') ?>   
+	<script>
+	function blockSpecialChar(e){
         var k;
         document.all ? k = e.keyCode : k = e.which;
         return ((k > 64 && k < 91) || (k > 96 && k < 123) || k == 8 || k == 32 || (k >= 48 && k <= 57));
