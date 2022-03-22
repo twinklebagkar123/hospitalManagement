bc819ed324dd27dbae2e1b724eaa33b1141a44ec
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-03-22 17:34:42 +0530

    test

--- hms/laboratory/include/sidebar.php
+++ hms/laboratory/include/sidebar.php
@@ -6,17 +6,21 @@
                 <span>Main Navigation</span>
             </div>
             <ul class="main-navigation-menu">
-                <li>
-                    <a href="dashboard.php">
-                        <div class="item-content">
-                            <div class="item-media">
-                                <i class="ti-home"></i>
+                <li><?php
+                    if (basename($_SERVER['PHP_SELF']) == "dashboard.php") { ?>
+                        <a href="#">
+                        <?php } else { ?>
+                            <a href="../dashboard.php">
+                            <?php } ?>
+                            <div class="item-content">
+                                <div class="item-media">
+                                    <i class="ti-home"></i>
+                                </div>
+                                <div class="item-inner">
+                                    <span class="title"> Dashboard </span>
+                                </div>
                             </div>
-                            <div class="item-inner">
-                                <span class="title"> Dashboard </span>
-                            </div>
-                        </div>
-                    </a>
+                            </a>
                 </li>
                 <li>
                     <a href="viewCompletedTests.php">
@@ -61,7 +65,7 @@
                                 <i class="ti-list"></i>
                             </div>
                             <div class="item-inner">
-                                <span class="title"> Pending Tests  </span>
+                                <span class="title"> Pending Tests </span>
                             </div>
                         </div>
                     </a>
