4263c6e853139d0bad9e2159f096b00b2e194e2b
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-03-23 18:40:32 +0530

    test

--- hms/laboratory/pages/temp.php
+++ hms/laboratory/pages/temp.php
@@ -1,6 +1,12 @@
-
 <?php
-include('../include/header.php');
+session_start();
+error_reporting(0);
+include('include/config.php');
+include('include/checklogin.php');
+check_login();
+?>
+<?php
+
 if (isset($_POST['submit'])) {
     $testID = $_GET['testID'];
     $recID = $_GET['recID'];
@@ -24,10 +30,33 @@ if (isset($_POST['submit'])) {
     $date = date("Y-m-d");
     $testJson = json_encode($testresult);
     $updateQuery = "UPDATE `labTestRecord` SET `labTestStatus`='complete',`testResult`='$testJson',`findings`='$findings',`performedDate`='$date',`performedBy`='$performedBy',`charges`='$charges' WHERE recordID = '$recID'";
+    //print_r($updateQuery);
     $result = $con->query($updateQuery);
 }
 
+?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <title>Laboratory | View Tests</title>
+
+    <link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+    <link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+    <link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+    <link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+    <link href="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="screen">
+    <link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
+    <link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/plugins.css">
+    <link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+</head>
+<?php
 $adID = $_GET['adID'];
 $testID = $_GET['testID'];
 function fetchPatientName($admissionID)
@@ -44,7 +73,12 @@ function fetchPatientName($admissionID)
 
 ?>
 
-<
+<body>
+    <div id="app">
+        <?php include('include/sidebar.php'); ?>
+        <div class="app-content">
+            <?php include('include/header.php'); ?>
+            <div class="main-content">
                 <div class="wrap-content container" id="container">
                     <!-- start: PAGE TITLE -->
                     <section id="page-title">
@@ -195,11 +229,39 @@ function fetchPatientName($admissionID)
         </div>
     </div>
     <!-- start: FOOTER -->
-    <?php include('../include/footer.php'); ?>
+    <?php include('include/footer.php'); ?>
 
     <!-- end: FOOTER -->
 
+    <!-- start: SETTINGS -->
+    <?php include('include/setting.php'); ?>
+
+    <!-- end: SETTINGS -->
+    </div> 
+    <!-- start: MAIN JAVASCRIPTS -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="vendor/modernizr/modernizr.js"></script>
+    <script src="vendor/jquery-cookie/jquery.cookie.js"></script>
+    <script src="vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+    <script src="vendor/switchery/switchery.min.js"></script>
     
+    <!-- end: MAIN JAVASCRIPTS -->
+    <!-- start: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+    <script src="vendor/maskedinput/jquery.maskedinput.min.js"></script>
+    <script src="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.js"></script>
+    <script src="vendor/autosize/autosize.min.js"></script>
+    <script src="vendor/selectFx/classie.js"></script>
+    <script src="vendor/selectFx/selectFx.js"></script>
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+    <script src="vendor/bootstrap-timepicker/bootstrap-timepicker.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
+    <!-- end: JAVASCRIPTS REQUIRED FOR THIS PAGE ONLY -->
+    <!-- start: CLIP-TWO JAVASCRIPTS -->
+    <script src="assets/js/form-elements.js"></script>
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.6.0/chart.min.js"></script> -->
+    <script src="assets/js/main.js"></script>
     <script>
 
 
