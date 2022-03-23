7093daa484ec4bdcaa7df810601318f6cfe3191e
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-03-23 16:55:01 +0530

    Update viewCompletedTests.php

--- hms/laboratory/pages/viewCompletedTests.php
+++ hms/laboratory/pages/viewCompletedTests.php
@@ -44,7 +44,7 @@ function fetchPatientName($admissionID){
 											<th>Test Type</th>
 											<th>Patient Name</th>
 											<th>Status</th>
-											<th>Action</th>
+											
 											<th>Reports</th>
 										</thead>
 										<tbody id="viewReport">
@@ -58,21 +58,7 @@ function fetchPatientName($admissionID){
 													<td><?php echo $row['labTestName'];?></td>
 													<td><?php echo fetchPatientName($row['admissionID']) ;?></td>
 													<td><?php echo $row['labTestStatus']; ?></td>
-													<!-- <td>
-                                                    <?php // if($row['labTestStatus'] == "pending"){
-                                                        ?>
-                                                            <a href="performTest.php?recID=<?php //echo $row['recordID']?>&adID=<?php //echo $row['admissionID']; ?>&testID=<?php //echo $row['performedTestID'];?>">Perform test</a> | <a href="">Decline</a> 
-                                                        <?php
-                                                   // }?>    
-                                                    </td> -->
-													<td><?php
-                                                        if($row['labTestStatus'] == "complete"){
-                                                            ?>
-                                                             <a href="testResultReport.php?recID=<?php echo $row['recordID']?>">View Results</a>
-                                                            <?php
-                                                        }
-                                                    
-                                                    ?></td>
+													
 												</tr>
 											<?php
 												$sr++;
