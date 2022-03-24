2a8ce86dde2f4dbbe54ff7c5524ec31652ed8567
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-03-24 18:41:33 +0530

    Update performTest.php

--- hms/laboratory/pages/performTest.php
+++ hms/laboratory/pages/performTest.php
@@ -76,7 +76,7 @@ function fetchPatientName($admissionID)
                         $fields_arr = explode(",", $fields);
                         $i = 1;
                         foreach ($fields_arr as  $field) {
-                            if(!(($fields == "units") || ($fields == "referanceRange") ||($fields == "normalRange") ) )
+                            if(!(($fields =='"units":""') || ($fields == "referanceRange") ||($fields == "normalRange") ) )
                             {
                         ?>
                             <div class="form-group">
