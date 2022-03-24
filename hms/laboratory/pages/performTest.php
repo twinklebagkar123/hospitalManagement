4f761c3751c62713167e88647f70afd74e330be4
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-03-24 15:30:19 +0530

    Update performTest.php

--- hms/laboratory/pages/performTest.php
+++ hms/laboratory/pages/performTest.php
@@ -1,4 +1,3 @@
-
 <?php
 include('../include/header.php');
 include('../include/config.php');
@@ -11,16 +10,14 @@ if (isset($_POST['submit'])) {
     $result = $con->query($query);
     while ($row = mysqli_fetch_array($result)) {
         $fields = $row['labFields'];
-      
-       $fields_arr = explode(",", $fields);
-       foreach ($fields_arr as  $value) {
-           $str = str_replace(' ', '_', $value);
-           $postVAl = $_POST["$str"];
-           $testresult[$value] = $postVAl;
-          
-       }
-       $charges = $row['labCharges'];
 
+        $fields_arr = explode(",", $fields);
+        foreach ($fields_arr as  $value) {
+            $str = str_replace(' ', '_', $value);
+            $postVAl = $_POST["$str"];
+            $testresult[$value] = $postVAl;
+        }
+        $charges = $row['labCharges'];
     }
     $date = date("Y-m-d");
     $testJson = json_encode($testresult);
@@ -38,224 +35,216 @@ function fetchPatientName($admissionID)
     $result =  $con->query($query);
     while ($row = mysqli_fetch_array($result)) {
         $answer = $row['PatientName'];
-      
     }
     return $answer;
 }
 
 ?>
-                <div class="wrap-content container" id="container">
-                    <!-- start: PAGE TITLE -->
-                    <section id="page-title">
-                        <div class="row">
-                            <div class="col-sm-8">
-                                <h1 class="mainTitle">Laboratory | Perform Tests</h1>
+<div class="wrap-content container" id="container">
+    <!-- start: PAGE TITLE -->
+    <section id="page-title">
+        <div class="row">
+            <div class="col-sm-8">
+                <h1 class="mainTitle">Laboratory | Perform Tests</h1>
+            </div>
+            <ol class="breadcrumb">
+                <li>
+                    <span>Admin</span>
+                </li>
+                <li class="active">
+                    <span>View Tests</span>
+                </li>
+            </ol>
+        </div>
+    </section>
+    <div class="container-fluid container-fullw bg-white">
+        <div class="row">
+            <div class="col-sm-6">
+
+                <form method="POST" action="">
+                    <?php
+                    $query = "SELECT * FROM `laboratoryTestList` where labFormID= '$testID'";
+                    $result = $con->query($query);
+                    $fields_arr = "";
+                    while ($row = mysqli_fetch_array($result)) {
+
+                    ?>
+                        <h3> <?php echo $row['labTestName']; ?> | <?php echo fetchPatientName($adID); ?></h3>
+
+                        <?php
+                        $fields = $row['labFields'] ;
+                        $fields_arr = explode(",", $fields);
+                        $i = 1;
+                        foreach ($fields_arr as  $field) {
+                        ?>
+                            <div class="form-group">
+                                <label for="<?php echo $field; ?>">
+                                    <?php echo $field; ?>
+                                </label>
+
+                                <input type="text" id="field_lab_<?php echo $i; ?>" name="<?php echo $field; ?>" class="form-control" placeholder="Enter <?php echo $field; ?>">
+
+
                             </div>
-                            <ol class="breadcrumb">
-                                <li>
-                                    <span>Admin</span>
-                                </li>
-                                <li class="active">
-                                    <span>View Tests</span>
-                                </li>
-                            </ol>
-                        </div>
-                    </section>
-                    <div class="container-fluid container-fullw bg-white">
-                        <div class="row">
-                            <div class="col-sm-6">
 
-                                <form method="POST" action="">
-                                    <?php
-                                    $query = "SELECT * FROM `laboratoryTestList` where labFormID= '$testID'";
-                                    $result = $con->query($query);
-                                    $fields_arr="";
-                                    while ($row = mysqli_fetch_array($result)) {
-                                        
-                                    ?>
-                                        <h3> <?php echo $row['labTestName']; ?> | <?php echo fetchPatientName($adID); ?></h3>
+                        <?php
+                            $i++;
+                        } ?>
+                        <input type="hidden" name="field_lab_counter" id="field_lab_counter" value="<?php echo $i - 1; ?>">
+                    <?php
 
-                                        <?php
-                                        $fields = $row['labFields'].",".$row['main_titles'] ;
-                                        $fields_arr = explode(",", $fields);
-                                        $i=1;
-                                        foreach ($fields_arr as  $field) {
-                                        ?>
-                                            <div class="form-group">
-                                                <label for="<?php echo $field; ?>">
-                                                    <?php echo $field; ?>
-                                                </label>
-
-                                                <input type="text" id="field_lab_<?php echo $i; ?>" name="<?php echo $field; ?>" class="form-control" placeholder="Enter <?php echo $field; ?>">
-
-                                                   
-                                            </div>
-
-                                    <?php
-                                    $i++;
-                                        }?>
-                                        <input type="hidden" name="field_lab_counter" id="field_lab_counter" value="<?php echo $i-1;?>">
-                                        <?php
-
-                                    }
-                                    ?>
-                                        								<?php
+                    }
+                    ?>
+                    <?php
 
 
-                                                                        $titles = $row['main_titles'];
-                                                                        $titles_arr = explode(",", $titles);
-                                                                        $i=1;
+                    $titles = $row['main_titles'];
+                    $titles_arr = explode(",", $titles);
+                    $i = 1;
 
-                                                                        foreach ($titles_arr as  $title) {
+                    foreach ($titles_arr as  $title) {
 
-                                                                            ?> <div class="row">
-                                                                                <div class="col-sm-3 justify-content-center">
-                                                                                <?php
-                                                                            if(strpos ($title, '*')!==false) {
-                                                                                ?>
+                    ?> <div class="row">
+                            <div class="col-sm-3 justify-content-center">
+                                <?php
+                                if (strpos($title, '*') !== false) {
+                                ?>
 
-                                                                        <p class="field_style text-bold">	 </p>
-                                                                                <?php
+                                    <p class="field_style text-bold"> </p>
+                                <?php
 
 
 
-                                                                            }
-                                                                            else{
-                                                                        ?>
-                                                                            
-                                                                                <p class="field_style">	<?php echo $field; ?> </p>
-                                                                            
-                                                                        <?php
-                                                                            }
-                                                                            ?>
-                                                                            </div>
-                                                                            </div>
+                                } else {
+                                ?>
 
-                                                                        <?php
-                                                                        $i++;
-                                                                        }?>
-                                    <div class="form-group">
-                                        <label for="performedBy">
-                                           Laboratory Incharge
-                                        </label>
-                                        <input type="text" id="performedBy" name="performedBy" class="form-control" required="true">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="findings">
-                                           Laboratory Findings
-                                        </label>
-                                        <input type="text" id="findings" name="findings" class="form-control" required="true">
-                                    </div>
-                                    <input type="submit" class="btn btn-o btn-primary" name="submit" value="SUBMIT">
-                                </form>
-                            </div>
-                            <div class="col-sm-6">
+                                    <p class="field_style"> <?php echo $field; ?> </p>
 
-                            <a class="btn btn-o btn-primary" id ="addTestfield">+ Add Test</a>
-
+                                <?php
+                                }
+                                ?>
                             </div>
                         </div>
-                        <div class="container-fluid container-fullw bg-white">
 
-
-								<div class="row">
-									<div class="col-md-12">
-										<h5 class="over-title margin-bottom-15">Manage <span class="text-bold">Procedures</span></h5>
-										<p style="color:red;"><?php echo htmlentities($_SESSION['msg']); ?>
-											<?php echo htmlentities($_SESSION['msg'] = ""); ?></p>
-										<table class="table table-hover" id="table_field">
-											<thead>
-												<tr>
-													<?php
-                                                    foreach ($fields_arr as  $field) {
-                                                        echo "<th>$field<th>";
-                                                    }
-                                                    
-                                                    ?>
-                                                    
-													
-													<th>Action</th>
-
-												</tr>
-											</thead>
-											<tbody id="fieldShow">
-											
-
-
-											</tbody>
-										</table>
-									</div>
-								 
-								</div>
-							</div>
+                    <?php
+                        $i++;
+                    } ?>
+                    <div class="form-group">
+                        <label for="performedBy">
+                            Laboratory Incharge
+                        </label>
+                        <input type="text" id="performedBy" name="performedBy" class="form-control" required="true">
                     </div>
+                    <div class="form-group">
+                        <label for="findings">
+                            Laboratory Findings
+                        </label>
+                        <input type="text" id="findings" name="findings" class="form-control" required="true">
+                    </div>
+                    <input type="submit" class="btn btn-o btn-primary" name="submit" value="SUBMIT">
+                </form>
+            </div>
+            <div class="col-sm-6">
+
+                <a class="btn btn-o btn-primary" id="addTestfield">+ Add Test</a>
+
+            </div>
+        </div>
+        <div class="container-fluid container-fullw bg-white">
+
+
+            <div class="row">
+                <div class="col-md-12">
+                    <h5 class="over-title margin-bottom-15">Manage <span class="text-bold">Procedures</span></h5>
+                    <p style="color:red;"><?php echo htmlentities($_SESSION['msg']); ?>
+                        <?php echo htmlentities($_SESSION['msg'] = ""); ?></p>
+                    <table class="table table-hover" id="table_field">
+                        <thead>
+                            <tr>
+                                <?php
+                                foreach ($fields_arr as  $field) {
+                                    echo "<th>$field<th>";
+                                }
+
+                                ?>
+
+
+                                <th>Action</th>
+
+                            </tr>
+                        </thead>
+                        <tbody id="fieldShow">
+
+
+
+                        </tbody>
+                    </table>
                 </div>
-           
-    <!-- start: FOOTER -->
-    <?php include('../include/footer.php'); ?>
 
-    <!-- end: FOOTER -->
+            </div>
+        </div>
+    </div>
+</div>
 
-    
-    <script>
+<!-- start: FOOTER -->
+<?php include('../include/footer.php'); ?>
 
-
-           
-			
-            $(document).ready(function(){
-                Main.init();
-			FormElements.init();
-            var jsonFieldDetails = [];
-                $("#addTestfield").on("click", function(){
-                var j=1;
-                 console.log("hello");
-				 var counter = $("#field_lab_counter").val();
-                 var data = "<tr>";
-                while (j<=counter){
+<!-- end: FOOTER -->
 
 
-                    var textName = $("#field_lab_"+j).val();
+<script>
+    $(document).ready(function() {
+        Main.init();
+        FormElements.init();
+        var jsonFieldDetails = [];
+        $("#addTestfield").on("click", function() {
+            var j = 1;
+            console.log("hello");
+            var counter = $("#field_lab_counter").val();
+            var data = "<tr>";
+            while (j <= counter) {
 
-                    data = data+"<td>" +textName+ "</td>";
-                    
-                    // $("#table_field #fieldShow").append(data);
-                    // // $("#fieldShow . tr").last(td);
-                    j++;
-             
-                }
-                data = data+"</tr>";
-                
-                $("#table_field").append(data);
-                // $("#fieldShow").append("#field_lab_"+j);
-                // var trow = "<tr><td>"+counter+j+"</td><td class='remove' data-name='"+counter+"'>X</td></tr>";
-				// $("#fieldShow").append(trow);
-				// jsonFieldDetails.push(counter);
-				// jsonFieldDetailsString = jsonFieldDetails.toString();
-				// $("#fieldArray").val(jsonFieldDetailsString);
 
-				
-				//console.log(jsonFieldDetails);
-				
-			});
-            $(document).on("click" ,".remove",function(){
-				var shanti = $(this).data("name");
-              var index = jsonFieldDetails.indexOf(shanti);
-			  if (index > -1) {
-				jsonFieldDetails.splice(index, 1);
-			}
-			  console.log(jsonFieldDetails);
-			  jsonFieldDetailsString = jsonFieldDetails.toString();
-				$("#fieldArray").val(jsonFieldDetailsString);
-			   $(this).parent().remove();
+                var textName = $("#field_lab_" + j).val();
 
-			});
-            });
-			
-			
+                data = data + "<td>" + textName + "</td>";
 
-    </script>
-    <!-- start: JavaScript Event Handlers for this page -->
-   
-   
+                // $("#table_field #fieldShow").append(data);
+                // // $("#fieldShow . tr").last(td);
+                j++;
+
+            }
+            data = data + "</tr>";
+
+            $("#table_field").append(data);
+            // $("#fieldShow").append("#field_lab_"+j);
+            // var trow = "<tr><td>"+counter+j+"</td><td class='remove' data-name='"+counter+"'>X</td></tr>";
+            // $("#fieldShow").append(trow);
+            // jsonFieldDetails.push(counter);
+            // jsonFieldDetailsString = jsonFieldDetails.toString();
+            // $("#fieldArray").val(jsonFieldDetailsString);
+
+
+            //console.log(jsonFieldDetails);
+
+        });
+        $(document).on("click", ".remove", function() {
+            var shanti = $(this).data("name");
+            var index = jsonFieldDetails.indexOf(shanti);
+            if (index > -1) {
+                jsonFieldDetails.splice(index, 1);
+            }
+            console.log(jsonFieldDetails);
+            jsonFieldDetailsString = jsonFieldDetails.toString();
+            $("#fieldArray").val(jsonFieldDetailsString);
+            $(this).parent().remove();
+
+        });
+    });
+</script>
+<!-- start: JavaScript Event Handlers for this page -->
+
+
 </body>
+
 </html>
