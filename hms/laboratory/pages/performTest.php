61c70dc079d1e3f8e654395b3dc1ac1b56aa9b81
BhaveshBagkar <89454856+BhaveshBagkar@users.noreply.github.com>
2022-03-24 19:00:11 +0530

    Update performTest.php

--- hms/laboratory/pages/performTest.php
+++ hms/laboratory/pages/performTest.php
@@ -72,7 +72,7 @@ function fetchPatientName($admissionID)
                         <h3> <?php echo $row['labTestName']; ?> | <?php echo fetchPatientName($adID); ?></h3>
 
                         <?php
-                        $fields = $row['labFields'];
+                        $fields = json_decode($row['labFields']);
                         // $fields_arr = explode(",", $fields);
                         $i = 1;
                         print_r($fields);
