b0b8212d81e83d33cb076872856b24facf4a2216
twinklebagkar123 <twinklebagkar99@gmail.com>
2022-03-31 12:03:38 +0530

    todays appointments

--- hms/doctor/todaysappointments.php
+++ hms/doctor/todaysappointments.php
@@ -50,7 +50,7 @@ if (isset($_GET['attend'])) {
 					<section id="page-title">
 						<div class="row">
 							<div class="col-sm-8">
-								<h1 class="mainTitle">Doctor | Appointment History</h1>
+								<h1 class="mainTitle">Doctor | Today's Appointments</h1>
 							</div>
 							<ol class="breadcrumb">
 								<li>
