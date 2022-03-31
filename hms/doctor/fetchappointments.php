c4cf4a15a56b8fb3ee0ce3f3e970899bc6751594
twinklebagkar123 <twinklebagkar99@gmail.com>
2022-03-31 16:38:18 +0530

    check

--- hms/doctor/fetchappointments.php
+++ hms/doctor/fetchappointments.php
@@ -4,5 +4,87 @@ include('include/config.php');
 include('include/checklogin.php');
 $id = $_POST['docID'];
 $appointmentdate = $_POST['appointmentdate'];
-echo $appointmentdate;
+
 ?>
+
+<table class="table table-hover" id="sample-table-1">
+	<thead>
+		<tr>
+			<th class="center">#</th>
+			<th class="hidden-xs">Patient Name</th>
+			<th>Specialization</th>
+			<th>Consultancy Fee</th>
+			<th>Appointment Date / Time </th>
+			<th>Appointment Creation Date </th>
+			<th>Current Status</th>
+			<th>Action</th>
+
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+		$query = "select tblpatient.PatientName as fname,appointment.*  from appointment join tblpatient on tblpatient.ID=appointment.userId where appointment.doctorId='" . $id . "' AND appointmentDate = '$appointmentdate'";
+		//print_r($query);
+		$sql = mysqli_query($con, $query);
+		$cnt = 1;
+		while ($row = mysqli_fetch_array($sql)) {
+		?>
+
+			<tr>
+				<td class="center"><?php echo $cnt; ?>.</td>
+				<td class="hidden-xs"><?php echo $row['fname']; ?></td>
+				<td><?php echo $row['doctorSpecialization']; ?></td>
+				<td><?php echo $row['consultancyFees']; ?></td>
+				<td><?php echo $row['appointmentDate']; ?> / <?php echo
+																$row['appointmentTime']; ?>
+				</td>
+				<td><?php echo $row['postingDate']; ?></td>
+				<td>
+					<?php if (($row['userStatus'] == 1) && ($row['doctorStatus'] == 1)) {
+						echo "Active";
+					}
+					if (($row['userStatus'] == 0) && ($row['doctorStatus'] == 1)) {
+						echo "Cancel by Patient";
+					}
+
+					if (($row['userStatus'] == 1) && ($row['doctorStatus'] == 0)) {
+						echo "Cancel by you";
+					}
+					if (($row['userStatus'] == 0) && ($row['doctorStatus'] == 0)) {
+						echo "Attending";
+					}
+
+
+
+					?></td>
+				<td>
+					<div class="visible-md visible-lg hidden-sm hidden-xs">
+						<?php if (($row['userStatus'] == 1) && ($row['doctorStatus'] == 1)) { ?>
+
+
+							<a href="appointment-history.php?id=<?php echo $row['id'] ?>&cancel=update" onClick="return confirm('Are you sure you want to cancel this appointment ?')" class="btn btn-transparent btn-xs tooltips" title="Cancel Appointment" tooltip-placement="top" tooltip="Remove">Cancel</a>
+						<?php } else {
+
+							echo "Canceled";
+						} ?>
+					</div>
+					<div class="visible-md visible-lg hidden-sm hidden-xs">
+						<?php if (($row['userStatus'] == 1) && ($row['doctorStatus'] == 0)) { ?>
+
+
+							<a href="appointment-history.php?id=<?php echo $row['id'] ?>&attend=update" onClick="return confirm('Are you sure you want to attend this appointment ?')" class="btn btn-transparent btn-xs tooltips" title="Attend Appointment" tooltip-placement="top" tooltip="Remove">Attend</a>
+						<?php } else {
+
+							echo "Attending";
+						} ?>
+					</div>
+				</td>
+			</tr>
+
+		<?php
+			$cnt = $cnt + 1;
+		} ?>
+
+
+	</tbody>
+</table>
