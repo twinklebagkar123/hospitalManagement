1afe0bedf1f6a640b9f49087d90d8c071ffdb46c
twinklebagkar123 <twinklebagkar99@gmail.com>
2022-03-31 11:52:25 +0530

    todays appointments

--- hms/doctor/include/sidebar.php
+++ hms/doctor/include/sidebar.php
@@ -35,6 +35,18 @@
 					</a>
 				</li>
 				<li>
+					<a href="todaysappointments.php">
+						<div class="item-content">
+							<div class="item-media">
+								<i class="ti-list"></i>
+							</div>
+							<div class="item-inner">
+								<span class="title"> Appointment History </span>
+							</div>
+						</div>
+					</a>
+				</li>
+				<li>
 					<a href="javascript:void(0)">
 						<div class="item-content">
 							<div class="item-media">
