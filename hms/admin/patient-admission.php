577687dda8258020838cca9f26ada0eb174cf8c9
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-03-31 18:43:54 +0530

    update

--- hms/admin/patient-admission.php
+++ hms/admin/patient-admission.php
@@ -28,7 +28,7 @@ $today = $year . '-' . $month . '-' . $day;
 	<link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
 	<link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
 	<link rel="stylesheet" href="assets/css/styles.css">
-	<link rel="stylesheet" href="assets/css/custom.css">
+	<link rel="stylesheet" href="assets/css/custom.css?ver=1">
 	<link rel="stylesheet" href="assets/css/plugins.css">
 	<link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
 
