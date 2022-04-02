7fcd61800780e479276d6a3e15e95ebd11136325
twinklebagkar123 <twinklebagkar99@gmail.com>
2022-04-02 11:21:05 +0530

    check

--- hms/doctor/fetchappointmentsbydate.php
+++ hms/doctor/fetchappointmentsbydate.php
@@ -7,7 +7,7 @@ $fromDate = $_POST['fromDate'];
 $toDate = $_POST['toDate'];
 
 ?>
-<h6>Your Appointments for the date: <?php echo $appointmentdate. " ";?> are: </h6>
+<h6>Your Appointments for the date: <?php echo $fromDate. " - ".$toDate ;?> are: </h6>
 <table class="table table-hover" id="sample-table-1">
 	<thead>
 		<tr>
@@ -24,7 +24,7 @@ $toDate = $_POST['toDate'];
 	</thead>
 	<tbody>
 		<?php
-		$query = "select tblpatient.PatientName as fname,appointment.*  from appointment join tblpatient on tblpatient.ID=appointment.userId where appointment.doctorId='" . $id . "' AND appointmentDate = '$appointmentdate'";
+		$query = "select tblpatient.PatientName as fname,appointment.*  from appointment join tblpatient on tblpatient.ID=appointment.userId where appointment.doctorId='" . $id . "'";
 		print_r($query);
 		//$sql = mysqli_query($con, $query);
 		$cnt = 1;
