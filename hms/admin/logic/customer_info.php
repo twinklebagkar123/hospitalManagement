7b514b9d12a4878e4c94b829641c6d498667e541
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-04-03 12:44:01 +0530

    update

--- hms/admin/logic/customer_info.php
+++ hms/admin/logic/customer_info.php
@@ -4,17 +4,17 @@ $con;
 $inputKey = $_POST['inputKey'];
 $searchBy = $_POST['searchBy'];
 function getCustomerDetails($inputKey,$searchBy){
-    switch ($searchBy) {
+    switch ($inputKey) {
         case 'id':
             global $con;
-            $sql = "SELECT `PatientName` FROM tblpatient WHERE ID='$inputKey'";
+            $sql = "SELECT `PatientName` FROM tblpatient WHERE ID='$searchBy'";
             $result = mysqli_fetch_array(mysqli_query($con,$sql));
             $arrayResponse = array('patientID' => $inputKey,'patientName'=> $result[0]['PatientName']);
             return json_encode($arrayResponse);
             break;
         case 'contact':
             global $con;
-            $sql = "SELECT `ID`,`PatientName` FROM tblpatient WHERE PatientContno='$inputKey'";
+            $sql = "SELECT `ID`,`PatientName` FROM tblpatient WHERE PatientContno='$searchBy'";
             $result = mysqli_query($con,$sql);
             while ($row = mysqli_fetch_array($result)) {
                 return $row['PatientName'].$inputKey."  ".$searchBy;
@@ -25,10 +25,11 @@ function getCustomerDetails($inputKey,$searchBy){
         
         default:
             global $con;
-            $sql = "SELECT `ID`,`PatientName` FROM tblpatient WHERE adharCardNo='$inputKey'";
+            $sql = "SELECT `ID`,`PatientName` FROM tblpatient WHERE adharCardNo='$searchBy'";
             $result = mysqli_fetch_array(mysqli_query($con,$sql));
-            $arrayResponse = array('patientID' => $result[0]['ID'],'patientName'=> $result[0]['PatientName']);
-            return json_encode($arrayResponse);
+            // $arrayResponse = array('patientID' => $result[0]['ID'],'patientName'=> $result[0]['PatientName']);
+            echo $searchBy;
+            // return json_encode($arrayResponse);
             break;
     }
 }
