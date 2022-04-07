09d7e5d4253e26853ebf662be49e03af65999beb
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-04-07 16:52:04 +0530

    update

--- hms/admin/logic/adhar_verify.php
+++ hms/admin/logic/adhar_verify.php
@@ -6,7 +6,8 @@ $result = mysqli_fetch_array(mysqli_query($con,$sql));
 if($result['ID'] == '' || $result['ID'] == null):
     $arrayResponse = array('patientID' => null,'patientName'=> null);
     return json_encode($arrayResponse);
+else:
+        $arrayResponse = array('patientID' => $result['ID'],'patientName'=> $result['PatientName']);
+        return json_encode($arrayResponse);
 endif;
-$arrayResponse = array('patientID' => $result['ID'],'patientName'=> $result['PatientName']);
-return json_encode($arrayResponse);
 ?>
