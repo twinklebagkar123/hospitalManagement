8fd373170cb4cdeaaf1be103495e39df522842bc
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-04-08 21:38:25 +0530

    update: multiple ocntact handle admission

--- hms/admin/logic/customer_info.php
+++ hms/admin/logic/customer_info.php
@@ -9,21 +9,31 @@ function getCustomerDetails($inputKey,$searchBy){
             global $con;
             $sql = "SELECT `PatientName` FROM tblpatient WHERE ID='$searchBy'";
             $result = mysqli_fetch_array(mysqli_query($con,$sql));
-            $arrayResponse = array('patientID' => $inputKey,'patientName'=> $result['PatientName']);
+            $arrayResponse = array('patientID' => $inputKey,'patientName'=> $result['PatientName'],'multiResult' => false);
             return json_encode($arrayResponse);
             break;
         case 'contact':
             global $con;
             $sql = "SELECT `ID`,`PatientName` FROM tblpatient WHERE PatientContno='$searchBy'";
-            $result = mysqli_fetch_array(mysqli_query($con,$sql));
-            $arrayResponse = array('patientID' => $result['ID'],'patientName'=> $result['PatientName']);
-            return json_encode($arrayResponse);
+            $mysqlResult = mysqli_query($con,$sql);
+            $num=mysqli_num_rows($mysqlResult);
+            if($num > 1){
+                $allResults = [];
+                while($row=mysqli_fetch_array($mysqlResult)){
+                    $output = array('patientID' => $row['ID'],'patientName'=> $row['PatientName'],'multiResult' => true);
+                    array_push($allResults,$output);
+                }
+                return json_encode($allResults);
+            }else{
+                $row=mysqli_fetch_array($mysqlResult);
+                return json_encode(array('patientID' => $row['ID'],'patientName'=> $row['PatientName'],'multiResult' => false));
+            }
             break;
         default:
             global $con;
             $sql = "SELECT `ID`,`PatientName` FROM tblpatient WHERE adharCardNo='$searchBy'";
             $result = mysqli_fetch_array(mysqli_query($con,$sql));
-            $arrayResponse = array('patientID' => $result['ID'],'patientName'=> $result['PatientName']);
+            $arrayResponse = array('patientID' => $result['ID'],'patientName'=> $result['PatientName'],'multiResult' => false);
             return json_encode($arrayResponse);
             break;
     }
--- hms/admin/patient-admission.php
+++ hms/admin/patient-admission.php
@@ -201,7 +201,7 @@ $today = $year . '-' . $month . '-' . $day;
 			<div class="modal-dialog modal-dialog-centered" role="document">
 				<div class="modal-content">
 					<div class="modal-header">
-						<h5 class="modal-title" id="exampleModalCenterTitle">Choose One Patient from below:</h5>
+						<h5 class="modal-title" id="exampleModalCenterTitle" style="display: inline-block;">Choose One Patient from below:</h5>
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 							<span aria-hidden="true">&times;</span>
 						</button>
@@ -234,7 +234,7 @@ $today = $year . '-' . $month . '-' . $day;
 					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-						<button type="button" class="btn btn-primary">Save changes</button>
+						<button type="button" class="btn btn-primary">Confirm</button>
 					</div>
 				</div>
 			</div>
