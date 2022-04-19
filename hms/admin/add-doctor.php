295164fc47b1c95d5916fc2d8d98a83263ab7d1c
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-04-19 13:56:08 +0530

    update

--- hms/admin/add-doctor.php
+++ hms/admin/add-doctor.php
@@ -8,12 +8,11 @@ check_login();
 if (isset($_POST['submit'])) {
 	$docspecialization = $_POST['Doctorspecialization'];
 	$docname = $_POST['docname'];
-	$docaddress = $_POST['clinicaddress'];
 	$docfees = $_POST['docfees'];
 	$doccontactno = $_POST['doccontact'];
 	$docemail = $_POST['docemail'];
 	$password = md5($_POST['npass']);
-	$sql = mysqli_query($con, "insert into doctors(specilization,doctorName,address,docFees,contactno,docEmail,password) values('$docspecialization','$docname','$docaddress','$docfees','$doccontactno','$docemail','$password')");
+	$sql = mysqli_query($con, "insert into doctors(specilization,doctorName,docFees,contactno,docEmail,password) values('$docspecialization','$docname','$docfees','$doccontactno','$docemail','$password')");
 	if ($sql) {
 		echo "<script>alert('Doctor info added Successfully');</script>";
 		echo "<script>window.location.href ='manage-doctors.php'</script>";
@@ -132,14 +131,6 @@ if (isset($_POST['submit'])) {
 															Doctor Name
 														</label>
 														<input type="text" name="docname" class="form-control" placeholder="Enter Doctor Name" required="true">
-													</div>
-
-
-													<div class="form-group">
-														<label for="address">
-															Doctor Clinic Address
-														</label>
-														<textarea name="clinicaddress" class="form-control" placeholder="Enter Doctor Clinic Address" required="true"></textarea>
 													</div>
 													<div class="form-group">
 														<label for="fess">
