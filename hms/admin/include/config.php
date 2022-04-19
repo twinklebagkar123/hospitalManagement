340af259776c268f32c0588326c7ed5bf4aa4ee4
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-04-19 13:05:50 +0530

    update

--- hms/admin/include/config.php
+++ hms/admin/include/config.php
@@ -2,6 +2,7 @@
 define('DB_SERVER','localhost');
 define('DB_USER','u298126064_hospital');
 define('DB_PASS' ,'?mnR!Y!y0');
+date_default_timezone_set('Asia/Kolkata');
 define('DB_NAME', 'u298126064_hospital');
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 // Check connection
