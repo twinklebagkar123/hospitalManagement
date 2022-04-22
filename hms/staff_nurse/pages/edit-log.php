dfd27bf50c7e1d2c985d7ad32b23babd70f3f001
twinklebagkar123 <twinklebagkar99@gmail.com>
2022-04-22 23:16:23 +0530

    test

--- hms/staff_nurse/pages/edit-log.php
+++ hms/staff_nurse/pages/edit-log.php
@@ -22,38 +22,44 @@ check_login();
             </ol>
         </div>
     </section>
+    <?php 
+    $editid = $_GET['editid'];
+    $query = "SELECT * FROM `fluidintakelog` WHERE logid = '$editid'";
+    $queryResult = $con->query($query);
+    $row = mysqli_fetch_assoc($queryResult);
+    ?>
     <div class="container-fluid container-fullw bg-white">
         <div class="row">
             <div class="col-md-12">
                 <h5 class="over-title margin-bottom-15">Manage <span class="text-bold">Patients</span></h5>
             </div>
-            <div class="col-md-12">
+            <div class="col-md-6">
             <form method="post">
                             <div class="input">
                               <input type="hidden" value="" id="fluidadmissionID" name="admissionID">
                             <h5 class="panel-title">INPUT</h5>
                               <div class="form-group">
                                 <label>IV</label>
-                                <input name="iv" placeholder="IV" class="form-control wd-450" required="true">
+                                <input name="iv" placeholder="IV" class="form-control" value="<?php echo $row['iv'];?>">
                               </div>
                               <div class="form-group">
                                 <label>Oral</label>
-                                <input name="oral" placeholder="oral" class="form-control wd-450" required="true">
+                                <input name="oral" placeholder="oral" class="form-control"  value="<?php echo $row['oral'];?>">
                               </div>
                               <div class="form-group">
                                 <label>RT</label>
-                                <input name="rt" placeholder="rt" class="form-control wd-450" required="true">
+                                <input name="rt" placeholder="rt" class="form-control"  value="<?php echo $row['rt'];?>">
                               </div>
                             </div>
                             <div class="output">
                             <h5 class="panel-title">OUTPUT</h5>
                               <div class="form-group">
                               <label>Urine</label>
-                              <input name="urine" placeholder="urine" class="form-control wd-450" required="true">
+                              <input name="urine" placeholder="urine" class="form-control"  value="<?php echo $row['urine'];?>">
                               </div>
                               <div class="form-group">
                               <label>Others</label>
-                              <input name="others" placeholder="others" class="form-control wd-450" required="true">
+                              <input name="others" placeholder="others" class="form-control"  value="<?php echo $row['others'];?>">
                               </div>
                             </div>
                             <div>
