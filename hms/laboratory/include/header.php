fe9a0abaa346ac812705631413dc3622f64f32ac
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-04-24 13:43:56 +0530

    update

--- hms/laboratory/include/header.php
+++ hms/laboratory/include/header.php
@@ -10,22 +10,40 @@ check_login();
 
 <head>
     <title>Laboratory | Dashboard</title>
+    <?php if (basename($_SERVER['PHP_SELF']) == "dashboard.php") { ?>
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="vendor/themify-icons/themify-icons.min.css">
+        <link href="vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+        <link href="vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+        <link href="vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+        <link href="vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
+        <link href="vendor/select2/select2.min.css" rel="stylesheet" media="screen">
+        <link href="vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
+        <link href="vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
+        <link rel="stylesheet" href="assets/css/styles.css">
+        <link rel="stylesheet" href="assets/css/plugins.css">
+        <link rel="stylesheet" href="assets/css/themes/theme-1.css" id="skin_color" />
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+    <?php } else { ?>
+            <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
+            <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
+            <link rel="stylesheet" href="../vendor/fontawesome/css/font-awesome.min.css">
+            <link rel="stylesheet" href="../vendor/themify-icons/themify-icons.min.css">
+            <link href="../vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
+            <link href="../vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
+            <link href="../vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
+            <link href="../vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
+            <link href="../vendor/select2/select2.min.css" rel="stylesheet" media="screen">
+            <link href="../vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
+            <link href="../vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
+            <link rel="stylesheet" href="../assets/css/styles.css">
+            <link rel="stylesheet" href="../assets/css/plugins.css">
+            <link rel="stylesheet" href="../assets/css/themes/theme-1.css" id="skin_color" />
+            <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+        <?php } ?>
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
-    <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../vendor/fontawesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../vendor/themify-icons/themify-icons.min.css">
-    <link href="../vendor/animate.css/animate.min.css" rel="stylesheet" media="screen">
-    <link href="../vendor/perfect-scrollbar/perfect-scrollbar.min.css" rel="stylesheet" media="screen">
-    <link href="../vendor/switchery/switchery.min.css" rel="stylesheet" media="screen">
-    <link href="../vendor/bootstrap-touchspin/jquery.bootstrap-touchspin.min.css" rel="stylesheet" media="screen">
-    <link href="../vendor/select2/select2.min.css" rel="stylesheet" media="screen">
-    <link href="../vendor/bootstrap-datepicker/bootstrap-datepicker3.standalone.min.css" rel="stylesheet" media="screen">
-    <link href="../vendor/bootstrap-timepicker/bootstrap-timepicker.min.css" rel="stylesheet" media="screen">
-    <link rel="stylesheet" href="../assets/css/styles.css">
-    <link rel="stylesheet" href="../assets/css/plugins.css">
-    <link rel="stylesheet" href="../assets/css/themes/theme-1.css" id="skin_color" />
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
 
 </head>
 
