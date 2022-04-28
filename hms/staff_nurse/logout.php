913aa710ef0863f590368e0a29435afac32bb11f
bhushan012 <47252778+bhushan012@users.noreply.github.com>
2022-04-28 16:49:41 +0530

    update

--- hms/staff_nurse/logout.php
+++ hms/staff_nurse/logout.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
+include("include/config.php");
 $_SESSION['login']=="";
+$login_log_id = $_SESSION['staff_log_id'];
+$date = date("Y-m-d H:i:s");
+$con = mysqli_query($con, "update application_logs set logout='" . $date . "' where id='" . $login_log_id . "'");
 session_unset();
 session_destroy();
 ?>
