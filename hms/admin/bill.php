5045000b495fd61cc7bdd0adc8395913881a0bd2
twinklebagkar123 <twinklebagkar99@gmail.com>
2022-06-03 11:27:24 +0530

    test

--- hms/admin/bill.php
+++ hms/admin/bill.php
@@ -474,17 +474,7 @@ function getDoctorFees($docID)
         </div>
 
 
-        <?php
-        function random_color_part() {
-          $dt = '';
-          for($o=1;$o<=3;$o++)
-          {
-              $dt .= str_pad( dechex( mt_rand( 0, 127 ) ), 2, '0', STR_PAD_LEFT);
-          }
-          return $dt;
-      }
-      echo random_color_part();
-        ?>
+       
 
 
 
